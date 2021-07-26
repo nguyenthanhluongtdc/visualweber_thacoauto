eacefa7b48872ca1b34ab5cc44696dc178930fea
thuanho195 <boyover055@gmail.com>
2021-07-26 15:57:46 +0700

    sass

--- platform/themes/main/views/media-newspapers.blade.php
+++ platform/themes/main/views/media-newspapers.blade.php
@@ -11,14 +11,220 @@
         <p class="title-docx font-cond color-gray">TCBC_Thong tin ve viec THACO huy dang ky cong ty
             dai chung..docx</p>
         <p class="size-dowload">
-            <span class="font-cond color-gray">804,24KB</span>
+            <span class="left font-cond color-gray">804,24KB</span>
             <span class="font-pri"><a href="">DOWNLOAD</a></span>
         </p>
         <div class="tags">
-            <span class="font-pri-bold">Tags: </span>
-            <span class="tag-title font-pri font13">
-                <a href="">Lorem Isum</a>
-            </span>
+            <p class="name-tag font-pri-bold">Tags: </span>
+            <div class="tag-list">
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+            </div>
         </div>
     </div>
+    <div class="item-newspaper">
+        <h5 class="title font-pri-bold font30 color-gray">
+            <a href="">Thông tin v/v THACO hủy đăng ký công ty đại chúng</a>
+        </h5>
+        <p class="desc font-pri color-gray">
+            Ngày 14/05/2021, UBCKNN thông báo hủy tư cách công ty đại chúng đối với Công ty Cổ phần Ô tô Trường Hải (THACO) do từ ngày 01/01/2021, Luật Chứng khoán 2019 và các văn bản hướng dẫn đã có hiệu lực thi hành: Công ty cổ phần có vốn điều lệ đã góp từ 30 tỷ đồng trở lên và có tối thiểu là 10% số cổ phiếu có 
+            quyền biểu quyết do ít nhất 100 nhà đầu tư không phải là cổ đông lớn nắm giữ với cơ cấu cổ đông.
+        </p>
+        <p class="title-docx font-cond color-gray">TCBC_Thong tin ve viec THACO huy dang ky cong ty
+            dai chung..docx</p>
+        <p class="size-dowload">
+            <span class="left font-cond color-gray">804,24KB</span>
+            <span class="font-pri"><a href="">DOWNLOAD</a></span>
+        </p>
+        <div class="tags">
+            <p class="name-tag font-pri-bold">Tags: </span>
+            <div class="tag-list">
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+            </div>
+        </div>
+    </div>
+    <div class="item-newspaper">
+        <h5 class="title font-pri-bold font30 color-gray">
+            <a href="">Thông tin v/v THACO hủy đăng ký công ty đại chúng</a>
+        </h5>
+        <p class="desc font-pri color-gray">
+            Ngày 14/05/2021, UBCKNN thông báo hủy tư cách công ty đại chúng đối với Công ty Cổ phần Ô tô Trường Hải (THACO) do từ ngày 01/01/2021, Luật Chứng khoán 2019 và các văn bản hướng dẫn đã có hiệu lực thi hành: Công ty cổ phần có vốn điều lệ đã góp từ 30 tỷ đồng trở lên và có tối thiểu là 10% số cổ phiếu có 
+            quyền biểu quyết do ít nhất 100 nhà đầu tư không phải là cổ đông lớn nắm giữ với cơ cấu cổ đông.
+        </p>
+        <p class="title-docx font-cond color-gray">TCBC_Thong tin ve viec THACO huy dang ky cong ty
+            dai chung..docx</p>
+        <p class="size-dowload">
+            <span class="left font-cond color-gray">804,24KB</span>
+            <span class="font-pri"><a href="">DOWNLOAD</a></span>
+        </p>
+        <div class="tags">
+            <p class="name-tag font-pri-bold">Tags: </span>
+            <div class="tag-list">
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+            </div>
+        </div>
+    </div>
+    <div class="item-newspaper">
+        <h5 class="title font-pri-bold font30 color-gray">
+            <a href="">Thông tin v/v THACO hủy đăng ký công ty đại chúng</a>
+        </h5>
+        <p class="desc font-pri color-gray">
+            Ngày 14/05/2021, UBCKNN thông báo hủy tư cách công ty đại chúng đối với Công ty Cổ phần Ô tô Trường Hải (THACO) do từ ngày 01/01/2021, Luật Chứng khoán 2019 và các văn bản hướng dẫn đã có hiệu lực thi hành: Công ty cổ phần có vốn điều lệ đã góp từ 30 tỷ đồng trở lên và có tối thiểu là 10% số cổ phiếu có 
+            quyền biểu quyết do ít nhất 100 nhà đầu tư không phải là cổ đông lớn nắm giữ với cơ cấu cổ đông.
+        </p>
+        <p class="title-docx font-cond color-gray">TCBC_Thong tin ve viec THACO huy dang ky cong ty
+            dai chung..docx</p>
+        <p class="size-dowload">
+            <span class="left font-cond color-gray">804,24KB</span>
+            <span class="font-pri"><a href="">DOWNLOAD</a></span>
+        </p>
+        <div class="tags">
+            <p class="name-tag font-pri-bold">Tags: </span>
+            <div class="tag-list">
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+            </div>
+        </div>
+    </div>
+    <div class="item-newspaper">
+        <h5 class="title font-pri-bold font30 color-gray">
+            <a href="">Thông tin v/v THACO hủy đăng ký công ty đại chúng</a>
+        </h5>
+        <p class="desc font-pri color-gray">
+            Ngày 14/05/2021, UBCKNN thông báo hủy tư cách công ty đại chúng đối với Công ty Cổ phần Ô tô Trường Hải (THACO) do từ ngày 01/01/2021, Luật Chứng khoán 2019 và các văn bản hướng dẫn đã có hiệu lực thi hành: Công ty cổ phần có vốn điều lệ đã góp từ 30 tỷ đồng trở lên và có tối thiểu là 10% số cổ phiếu có 
+            quyền biểu quyết do ít nhất 100 nhà đầu tư không phải là cổ đông lớn nắm giữ với cơ cấu cổ đông.
+        </p>
+        <p class="title-docx font-cond color-gray">TCBC_Thong tin ve viec THACO huy dang ky cong ty
+            dai chung..docx</p>
+        <p class="size-dowload">
+            <span class="left font-cond color-gray">804,24KB</span>
+            <span class="font-pri"><a href="">DOWNLOAD</a></span>
+        </p>
+        <div class="tags">
+            <p class="name-tag font-pri-bold">Tags: </span>
+            <div class="tag-list">
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+            </div>
+        </div>
+    </div>
+    <div class="item-newspaper">
+        <h5 class="title font-pri-bold font30 color-gray">
+            <a href="">Thông tin v/v THACO hủy đăng ký công ty đại chúng</a>
+        </h5>
+        <p class="desc font-pri color-gray">
+            Ngày 14/05/2021, UBCKNN thông báo hủy tư cách công ty đại chúng đối với Công ty Cổ phần Ô tô Trường Hải (THACO) do từ ngày 01/01/2021, Luật Chứng khoán 2019 và các văn bản hướng dẫn đã có hiệu lực thi hành: Công ty cổ phần có vốn điều lệ đã góp từ 30 tỷ đồng trở lên và có tối thiểu là 10% số cổ phiếu có 
+            quyền biểu quyết do ít nhất 100 nhà đầu tư không phải là cổ đông lớn nắm giữ với cơ cấu cổ đông.
+        </p>
+        <p class="title-docx font-cond color-gray">TCBC_Thong tin ve viec THACO huy dang ky cong ty
+            dai chung..docx</p>
+        <p class="size-dowload">
+            <span class="left font-cond color-gray">804,24KB</span>
+            <span class="font-pri"><a href="">DOWNLOAD</a></span>
+        </p>
+        <div class="tags">
+            <p class="name-tag font-pri-bold">Tags: </span>
+            <div class="tag-list">
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+                    <p class="tag-title font-pri font13">
+                        <a href="">Lorem Isum</a>
+                    </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="page-pagination">
+        <ul class="pagination font25 font-pri-bold">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+            <li>9</li>
+            <li>10</li>
+            <li class="color">></li>
+            <li class="color">>></li>
+        </ul>
+     </div>
 </div>

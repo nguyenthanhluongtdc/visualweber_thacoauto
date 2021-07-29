aef5c1956fccb99db123845071086061c7cd6ba5
ASUS-UX430\kHA <kha.ph@visualweber.com>
2021-07-29 14:09:45 +0700

    sass

---
+++ platform/themes/main/views/cultural.blade.php
@@ -0,0 +1,3 @@
+@includeIf("theme.main::views.pages.cultural.cultural-behavior")
+@includeIf("theme.main::views.pages.cultural.criterion")
+@includeIf("theme.main::views.pages.cultural.distribution-company")

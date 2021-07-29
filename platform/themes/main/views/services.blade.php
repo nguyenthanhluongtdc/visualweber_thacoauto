2874066d2d90857f9e8be538b6cc7dc870d46726
ASUS-UX430\kHA <kha.ph@visualweber.com>
2021-07-29 15:02:43 +0700

    sass

---
+++ platform/themes/main/views/services.blade.php
@@ -0,0 +1,32 @@
+<div class="services">
+    <div class="container-remake">
+        <div class="services-block">
+            <img class="services-block__image" src="" alt="">
+            <div class="services-block__title">
+                BẢO HÀNH BẢO DƯỠNG
+            </div>
+            <div class="services-block__desc">
+                Để đảm bảo chiếc xe của bạn luôn trong tình trạng hoàn hảo và an toàn, việc bảo dưỡng định kỳ được thực hiện bởi kỹ thuật viên được đào tạo chuyên nghiệp, phụ tùng chính hãng chất lượng cao, THACO có thể cung cấp dịch vụ bảo dưỡng cũng như chế độ bảo hành phù hợp cho xe bạn.
+            </div>
+            <a class="services-block__button">XEM CHI TIẾT</a>
+        </div>
+        <div class="services-block">
+            <img class="services-block__image" src="" alt="">
+            <div class="services-block__title">
+                SỬA CHỮA Ô TÔ
+            </div>
+            <div class="services-block__desc">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.            </div>
+            <a class="services-block__button">XEM CHI TIẾT</a>
+        </div>
+        <div class="services-block">
+            <img class="services-block__image" src="" alt="">
+            <div class="services-block__title">
+                CHĂM SÓC XE
+            </div>
+            <div class="services-block__desc">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.            </div>
+            <a class="services-block__button">XEM CHI TIẾT</a>
+        </div>
+    </div>
+</div>

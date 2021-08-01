fa203fb31201251454695fe7498589e9fcefdb99
Anonymous M2 <anonymous-M2@visualweber.com>
2021-08-01 17:12:27 +0700

    big upgrade 5.19

--- platform/plugins/blog/src/Forms/PostForm.php
+++ platform/plugins/blog/src/Forms/PostForm.php
@@ -37,14 +37,6 @@ class PostForm extends FormAbstract
                 ->all();
         }
 
-        $selectProvinces =[];
-        $listProvinces = get_all_provinces();
-        if (!empty($listProvinces)) {
-            foreach ($listProvinces as $row) {
-                $selectProvinces[] = $row->name;
-            }
-        }
-
         $tags = null;
 
         if ($this->getModel()) {
@@ -97,19 +89,6 @@ class PostForm extends FormAbstract
                 'label_attr' => ['class' => 'control-label required'],
                 'choices'    => BaseStatusEnum::labels(),
             ])
-            ->add('format_type', 'customRadio', [
-                'label'      => trans('plugins/blog::posts.form.format_type'),
-                'label_attr' => ['class' => 'control-label'],
-                'choices'    => get_post_formats(true),
-            ])
-            ->add('company', 'customSelect', [
-                'label'      => __('Công ty'),
-                'label_attr' => ['class' => 'control-label required'],
-                'attr'       => [
-                    'class' => 'select-search-full',
-                ],
-                'choices'    => $selectProvinces,
-            ])
             ->add('categories[]', 'categoryMulti', [
                 'label'      => trans('plugins/blog::posts.form.categories'),
                 'label_attr' => ['class' => 'control-label required'],
@@ -130,5 +109,15 @@ class PostForm extends FormAbstract
                 ],
             ])
             ->setBreakFieldPoint('status');
+
+        $postFormats = get_post_formats(true);
+
+        if (count($postFormats) > 1) {
+            $this->addAfter('status', 'format_type', 'customRadio', [
+                'label'      => trans('plugins/blog::posts.form.format_type'),
+                'label_attr' => ['class' => 'control-label'],
+                'choices'    => get_post_formats(true),
+            ]);
+        }
     }
 }

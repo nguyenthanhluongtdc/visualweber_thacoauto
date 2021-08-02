5ed3f513e5f43c0d20c5bd53477f6be4eb7cab7b
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-08-02 16:50:59 +0700

    sass

--- platform/themes/main/views/production-assemble.blade.php
+++ platform/themes/main/views/production-assemble.blade.php
@@ -1,31 +1,202 @@
 <div id="production-assemble-page">
-    <div class="section-card">
+    <div class="container-remake">
+        <div class="page__header my-90">
+            <h1 class="font-pri-bold font60 text-uppercase">sản xuất & lắp ráp</h1>
+        </div>
+    </div>
+    <div class="section-card-wrapper">
         <div class="container-remake">
-            <div class="section-card__header">
-
-            </div>
-
-            <div class="section-card__body">
-                <div class="section-card__row">
-                    <div class="section-card__row__picture">
-                        <img />
-                    </div>
-
-                    <div class="section-card__row__content">
-                        <div class="section-card__row__body">
-                            <h2 class="section-card__row__title">
-                        
-                            </h2>
-
-                            <p class="section-card__row__des">
-                                
-                            </p>
+            <div class="section-card">
+                <div class="section-card__body font-pri mb-90">
+                    <div class="section-card__row row">
+                        <div class="section-card__row__picture col-lg-8">
+                            <img width="" height="" src="{{Theme::asset()->url('images/manufacturing/assemble/assemble1.jpg')}}" alt="" />
                         </div>
 
-                        <div class="section-card__row__footer">
-                            <a class="section-card__row__footer__readmore">
-    
-                            </a>
+                        <div class="section-card__row__content col-lg-4 d-flex flex-wrap align-content-between">
+
+                            <div class="section-card__row__body">
+                                <div class="mb-3">
+                                    <div class="d-lg-block d-flex align-item-center">
+                                        <div class="number-order font30"> <div class="box"> 1 </div> </div>
+                                        <h3 class="section-card__row__body__title text-uppercase font-pri-bold font40">
+                                            NHÀ MÁY SẢN XUẤT <br> 
+                                            <span class="color-pri">XE DU LỊCH CAO CẤP</span> THACO
+                                        </h3>
+                                    </div>
+
+                                    <p class="section-card__row__body__des font20">
+                                        Thực hiện chiến lược đầu tư nâng cấp các nhà máy lắp ráp ô tô theo hướng tự động hóa và ứng dụng số hóa trong quản trị để sản xuất hàng loạt theo yêu cầu riêng biệt của khách hàng, Nhà máy sản xuất xe du lịch cao cấp Thaco được đầu tư nâng cấp toàn diện và chính thức đưa vào hoạt động từ tháng 5/2019. Nhà máy có công suất 20.000 xe/năm, diện tích 7.5 ha, vốn đầu tư 4.500 tỷ đồng.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="section-card__row__footer">
+                                <a href="#" title="{!! __('See detail') !!}" class="btn-read-more">
+                                    {!! __('See detail') !!}
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="section-card__body font-pri mb-90">
+                    <div class="section-card__row row">
+                        <div class="section-card__row__picture col-lg-8">
+                            <img width="" height="" src="{{Theme::asset()->url('images/manufacturing/assemble/assemble2.jpg')}}" alt="" />
+                        </div>
+
+                        <div class="section-card__row__content col-lg-4 d-flex flex-wrap align-content-between">
+
+                            <div class="section-card__row__body">
+                                <div class="mb-3">
+                                     <div class="d-lg-block d-flex align-item-center">
+                                        <div class="number-order font30"> <div class="box"> 2 </div> </div>
+                                        <h3 class="section-card__row__body__title text-uppercase font-pri-bold font40">
+                                            NHÀ MÁY THACO <span class="color-pri"> BUS </span> 
+                                        </h3>
+                                    </div>
+
+                                    <p class="section-card__row__body__des font20">
+                                        Nhà máy Bus Thaco được đưa vào hoạt động từ tháng 12/2017 với định vị là nhà máy sản xuất xe khách lớn nhất khu vực Đông Nam Á. Nhà máy được quy hoạch trong phân khu các nhà máy lắp ráp ô tô thuộc KCN THACO Chu Lai, có diện tích 17 ha, trong đó có 8 ha nhà xưởng; công suất 20.000 xe/năm (gồm: 8.000 xe bus và 12.000 xe mini bus).
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="section-card__row__footer">
+                                <a href="#" title="{!! __('See detail') !!}" class="btn-read-more">
+                                    {!! __('See detail') !!}
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="section-card__body font-pri mb-90">
+                    <div class="section-card__row row">
+                        <div class="section-card__row__picture col-lg-8">
+                            <img width="" height="" src="{{Theme::asset()->url('images/manufacturing/assemble/assemble3.jpg')}}" alt="" />
+                        </div>
+
+                        <div class="section-card__row__content col-lg-4 d-flex flex-wrap align-content-between">
+                            <div class="section-card__row__body">
+                                <div class="mb-3">
+                                   <div class="d-lg-block d-flex align-item-center">
+                                        <div class="number-order font30"> <div class="box"> 3 </div> </div>
+                                        <h3 class="section-card__row__body__title text-uppercase font-pri-bold font40">
+                                             NHÀ MÁY 
+                                             <span class="color-pri"> TẢI </span>  THACO
+                                        </h3>
+                                    </div>
+
+                                    <p class="section-card__row__body__des font20">
+                                        Thực hiện chiến lược đầu tư mới và nâng cấp các nhà máy sản xuất lắp ráp ô tô theo hướng tự động hóa, ứng dụng số hóa trong quản trị để sản xuất hàng loạt theo yêu cầu riêng biệt của khách hàng, đáp ứng nhu cầu thị trường trong nước và hướng đến xuất khẩu, tháng 02/2019, nhà máy sản xuất xe Tải Thaco được đầu tư xây dựng mới với công suất 50.000 xe/năm, diện tích 11ha, tổng vốn đầu tư 4.500 tỷ đồng. Nhà máy chính thức đi vào hoạt động từ tháng 03/2020.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="section-card__row__footer">
+                                <a href="#" title="{!! __('See detail') !!}" class="btn-read-more">
+                                    {!! __('See detail') !!}
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="section-card__body font-pri mb-90">
+                    <div class="section-card__row row">
+                        <div class="section-card__row__picture col-lg-8">
+                            <img width="" height="" src="{{Theme::asset()->url('images/manufacturing/assemble/assemble4.jpg')}}" alt="" />
+                        </div>
+
+                        <div class="section-card__row__content col-lg-4 d-flex flex-wrap align-content-between">
+                            <div class="section-card__row__body">
+                                <div class="mb-3">
+                                     <div class="d-lg-block d-flex align-item-center">
+                                        <div class="number-order font30"> <div class="box"> 4 </div> </div>
+                                        <h3 class="section-card__row__body__title text-uppercase font-pri-bold font40">
+                                           NHÀ MÁY THACO 
+                                           <span class="color-pri"> kia </span> 
+                                        </h3>
+                                    </div>
+
+                                    <p class="section-card__row__body__des font20">
+                                        Thực hiện chiến lược đầu tư mới và nâng cấp các nhà máy sản xuất lắp ráp ô tô nhằm nâng cao chất lượng sản phẩm, gia tăng tỷ lệ nội địa hóa, đáp ứng nhu cầu thị trường trong nước và xuất khẩu, Nhà máy Thaco Kia được đầu tư nâng cấp với sự tư vấn giải pháp và hỗ trợ kỹ thuật trực tiếp từ các kỹ sư, chuyên gia của Tập đoàn KIA Motors - Hàn Quốc và chính thức đưa vào hoạt động từ tháng 9/2019. Nhà máy có công suất 50.000 xe/năm, diện tích 31 ha với tổng vốn đầu tư 500 tỷ đồng.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="section-card__row__footer">
+                                <a href="#" title="{!! __('See detail') !!}" class="btn-read-more">
+                                    {!! __('See detail') !!}
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="section-card__body font-pri mb-90">
+                    <div class="section-card__row row">
+                        <div class="section-card__row__picture col-lg-8">
+                            <img width="" height="" src="{{Theme::asset()->url('images/manufacturing/assemble/assemble5.jpg')}}" alt="" />
+                        </div>
+
+                        <div class="section-card__row__content col-lg-4 d-flex flex-wrap align-content-between">
+                            <div class="section-card__row__body">
+                                <div class="mb-3">
+                                     <div class="d-lg-block d-flex align-item-center">
+                                        <div class="number-order font30"> <div class="box"> 5 </div> </div>
+                                        <h3 class="section-card__row__body__title text-uppercase font-pri-bold font40">
+                                            NHÀ MÁY THACO 
+                                            <span class="color-pri"> MAZDA </span>
+                                        </h3>
+                                    </div>
+
+                                    <p class="section-card__row__body__des font20">
+                                        Nhà máy Thaco Mazda được xây dựng và đưa vào hoạt động từ tháng 3/2018, là 1 trong 5 nhà máy sản xuất lắp ráp ô tô chủ lực của THACO tại KCN Cơ khí & Ô tô THACO Chu Lai, với định vị là nhà máy sản xuất xe du lịch Mazda hiện đại nhất khu vực ASEAN.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="section-card__row__footer">
+                                <a href="#" title="{!! __('See detail') !!}" class="btn-read-more">
+                                    {!! __('See detail') !!}
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="section-card__body font-pri mb-90">
+                    <div class="section-card__row row">
+                        <div class="section-card__row__picture col-lg-8">
+                            <img width="" height="" src="{{Theme::asset()->url('images/manufacturing/assemble/assemble4.jpg')}}" alt="" />
+                        </div>
+
+                        <div class="section-card__row__content col-lg-4 d-flex flex-wrap align-content-between">
+                            <div class="section-card__row__body">
+                                <div class="mb-3">
+                                     <div class="d-lg-block d-flex align-item-center">
+                                        <div class="number-order font30"> <div class="box"> 6 </div> </div>
+                                        <h3 class="section-card__row__body__title text-uppercase font-pri-bold font40">
+                                            NHÀ MÁY 
+                                            <span class="color-pri">SƠMI RƠMOOC </span>  VÀ THIẾT BỊ <br> CHUYÊN DỤNG 
+                                            <span class="color-pri"> HẠNG NẶNG </span>
+                                        </h3>
+                                    </div>
+
+                                    <p class="section-card__row__body__des font20">
+                                        Thực hiện chiến lược đầu tư nâng cấp các nhà máy lắp ráp ô tô theo hướng tự động hóa và ứng dụng số hóa trong quản trị để sản xuất hàng loạt theo yêu cầu riêng biệt của khách hàng, Nhà máy sản xuất xe du lịch cao cấp Thaco được đầu tư nâng cấp toàn diện và chính thức đưa vào hoạt động từ tháng 5/2019. Nhà máy có công suất 20.000 xe/năm, diện tích 7.5 ha, vốn đầu tư 4.500 tỷ đồng.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="section-card__row__footer">
+                                <a href="#" title="{!! __('See detail') !!}" class="btn-read-more">
+                                    {!! __('See detail') !!}
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>

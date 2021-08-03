e4c8ca5eced892678877af7bba7ae250f97cb953
Trúc Huỳnh <dntrh0425@gmail.com>
2021-08-03 17:30:51 +0700

    sass

--- platform/themes/main/views/contact.blade.php
+++ platform/themes/main/views/contact.blade.php
@@ -11,7 +11,7 @@
             {!! Form::open(['route' => 'public.send.contact', 'method' => 'POST']) !!}                   
                     <div class="form-group row">
                         <label for="" class="col-sm-3 text-label mt-3">NƠI GỬI ĐẾN:</label>
-                        <div class="col-sm-9">
+                        <div class="col-sm-6 pr-0">
                             <select name="" id="">
                                 <option value="">THACO AUTO</option>
                             </select>
@@ -21,7 +21,7 @@
                     <div class="form-group row">
                         <label for="" class="col-sm-3 text-label mt-3">HỌ TÊN:</label>
                         <div class="col-sm-9 row pr-0">
-                            <div class="col-sm-6">
+                            <div class="col-sm-6 pr-0">
                                 <input type="text" class="form-control" id="contact_name_first" placeholder="Họ*" name="first-name" value="{{ old('name') }}"> 
                             </div>
                             <div class="col-sm-6 pr-0">
@@ -85,11 +85,25 @@
                     <div class="col-md-8 detail-content-wrap">
                         
                             <h3 class="detail-title font40 font-pri-bold mt-4 mb-3">TRỤ SỞ CHÍNH CÔNG TY Ô TÔ TRƯỜNG HẢI TẠI TP. HỒ CHÍ MINH</h3>
-                            <p class="font-pri"><ion-icon name="location-outline"></ion-icon><span class="font-pri-bold">Địa chỉ: </span>Số 10 Mai Chí Thọ, P.Thủ Thiêm, TP.Thủ Đức, TP.HCM</p>
-                            <p class="font-pri"><ion-icon name="call-outline"></ion-icon><span class="font-pri-bold">Hotline: </span>1900545591</p>
-                            <p class="font-pri"><ion-icon name="print-outline"></ion-icon><span class="font-pri-bold">Fax: </span>+84 (0)8-39977.742</p>
-                            <p class="font-pri"><ion-icon name="at-outline"></ion-icon><span class="font-pri-bold">Website: </span>www.truonghaiauto.com.vn</p>
-                            <p class="font-pri"><ion-icon name="mail-outline"></ion-icon><span class="font-pri-bold">Email: </span>rep.office@thaco.com.vn</p>
+                            <div class="item">
+                                <ion-icon name="location-outline"></ion-icon><p class="font-pri"><span class="font-pri-bold">Địa chỉ: </span>Số 10 Mai Chí Thọ, P.Thủ Thiêm, TP.Thủ Đức, TP.HCM</p>
+                            </div>
+                           <div class="item">
+                            <ion-icon name="call-outline"></ion-icon><p class="font-pri"><span class="font-pri-bold">Hotline: </span>1900545591</p>
+                           </div>
+                           <div class="item">
+                            <ion-icon name="print-outline"></ion-icon><p class="font-pri"><span class="font-pri-bold">Fax: </span>+84 (0)8-39977.742</p>
+                            </div>
+                            <div class="item">
+                                <ion-icon name="at-outline"></ion-icon><p class="font-pri"><span class="font-pri-bold">Website: </span>www.truonghaiauto.com.vn</p>
+                            </div>
+                            <div class="item">
+                                <ion-icon name="mail-outline"></ion-icon><p class="font-pri"><span class="font-pri-bold">Email: </span>rep.office@thaco.com.vn</p>
+                            </div>
+                           
+                            
+                            
+                          
                     </div>
                 </div>
         </div>
@@ -105,10 +119,22 @@
                
                 <div class="col-md-8 detail-content-wrap">
                         <h3 class="detail-title font40 font-pri-bold mt-3 mb-3">VĂN PHÒNG THACO TẠI KHU PHỨC HỢP CHU LAI - TRƯỜNG HẢI</h3>
-                        <p class="font-pri"><ion-icon name="location-outline"></ion-icon><span class="font-pri-bold">Địa chỉ: </span>Thôn 4, Xã Tam Hiệp, Huyện Núi thành, Tỉnh Quảng Nam.</p>
-                        <p class="font-pri"><ion-icon name="call-outline"></ion-icon><span class="font-pri-bold">Hotline: </span>+84-0510.3567.161 - 0510.3567.162 - 0510.3567.163</p>
-                        <p class="font-pri"><ion-icon name="print-outline"></ion-icon><span class="font-pri-bold">Fax: </span> +84 - (0)510 - 3565777</p>
-                        <p class="font-pri"><ion-icon name="mail-outline"></ion-icon><span class="font-pri-bold">Email: </span> chulai-truonghai@dng.vnn.vn</p>
+                        <div class="item">
+                            <ion-icon name="location-outline"></ion-icon><p class="font-pri"><span class="font-pri-bold">Địa chỉ: </span>Thôn 4, Xã Tam Hiệp, Huyện Núi thành, Tỉnh Quảng Nam.</p>
+                        </div>
+                        <div class="item">
+                            <ion-icon name="call-outline"></ion-icon><p class="font-pri"><span class="font-pri-bold">Hotline: </span>+84-0510.3567.161 - 0510.3567.162 - 0510.3567.163</p>
+                        </div>
+                        <div class="item">
+                            <ion-icon name="print-outline"></ion-icon><p class="font-pri"><span class="font-pri-bold">Fax: </span> +84 - (0)510 - 3565777</p>
+                        </div>
+                        <div class="item">
+                            <ion-icon name="mail-outline"></ion-icon><p class="font-pri"><span class="font-pri-bold">Email: </span> chulai-truonghai@dng.vnn.vn</p>
+                        </div>
+                        
+                        
+                        
+                        
                 </div>
             </div>
     </div>
@@ -123,9 +149,18 @@
             </div>
             <div class="col-md-8 detail-content-wrap">
                     <h3 class="detail-title font40 font-pri-bold mt-3 mb-3">VĂN PHÒNG THACO TẠI HÀ NỘI</h3>
-                    <p class="font-pri"><ion-icon name="location-outline"></ion-icon><span class="font-pri-bold">Địa chỉ: </span>Lô D6, KCN Hà Nội Đài Tư, 386 Nguyễn Văn Linh, Sài Đồng, Long Biên, Hà Nội</p>
-                    <p class="font-pri"><ion-icon name="call-outline"></ion-icon><span class="font-pri-bold">Hotline: </span>+84 - (0)43.8758914</p>
-                    <p class="font-pri"><ion-icon name="print-outline"></ion-icon><span class="font-pri-bold">Fax: </span>043.8759857</p>
+                    <div class="item">
+                        <ion-icon name="location-outline"></ion-icon><p class="font-pri"><span class="font-pri-bold">Địa chỉ: </span>Lô D6, KCN Hà Nội Đài Tư, 386 Nguyễn Văn Linh, Sài Đồng, Long Biên, Hà Nội</p> 
+                    </div>
+                    <div class="item">
+                        <ion-icon name="call-outline"></ion-icon><p class="font-pri"><span class="font-pri-bold">Hotline: </span>+84 - (0)43.8758914</p> 
+                    </div>
+                    <div class="item">
+                        <ion-icon name="print-outline"></ion-icon><p class="font-pri"><span class="font-pri-bold">Fax: </span>043.8759857</p>
+                    </div>
+                   
+                    
+                    
             </div>
         </div>
 </div>

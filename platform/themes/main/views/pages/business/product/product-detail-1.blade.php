d53a5658eb3da9f296a14c8cc5ddbc40594afc45
Trúc Huỳnh <dntrh0425@gmail.com>
2021-08-05 16:25:35 +0700

    sass

--- platform/themes/main/views/pages/business/product/product-detail-1.blade.php
+++ platform/themes/main/views/pages/business/product/product-detail-1.blade.php
@@ -74,7 +74,7 @@
                     </div>
                     <h3 class="product-detail__card-title font25 font-mi-bold mt-4">TIỆN NGHI</h3>
                     <div class="product-detail__card-desc font20 font-pri">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo conse
                     </div>
                 </div>
             </div>

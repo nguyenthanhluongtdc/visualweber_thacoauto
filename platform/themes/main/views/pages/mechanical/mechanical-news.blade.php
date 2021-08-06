9c61352223ecf2cbf2390858a4171192ac63f07f
ThuanNguyen <nguyenbathuan98@gmail.com>
2021-08-06 17:20:55 +0700

    sass

---
+++ platform/themes/main/views/pages/mechanical/mechanical-news.blade.php
@@ -0,0 +1,93 @@
+<div class="section-list-news-wrapper mechandical">
+            <div class="container-remake">
+                <div class="section-list-news">
+                        <div class="section-list-news-header  d-flex justify-content-between align-items-center">
+                            <h2 class="title font-pri-bold font40 text-uppercase">
+                                Tin tức
+                            </h2>
+    
+                            <a href="#" title="{!!__('Read more')!!}" class="font20 text-uppercase read-more text-dark"> {!!__('Read more')!!} >> </a>
+                        </div>
+    
+                        <div class="content">
+                            <div class="item mx-0">
+                                <div class="item__left">
+                                    <div class="sub--left font-pri-bold">
+                                        <div class="day"> 29 </div>
+                                        <div class="month"> May </div>
+                                        <div class="year"> 2021 </div>
+                                    </div>
+    
+                                    <div class="sub--right">
+                                        <img width="" height="" src="{{Theme::asset()->url('images/manufacturing/manu8.jpg')}}" alt="" />
+                                    </div>
+                                </div>
+    
+                                <div class="item__right">
+                                    <div class="item__right__top">
+                                        <h3 class="font30 title"> Lợi ích của công nghệ cắt Laser thép ống, thép hộp </h3>
+                                        <p class="description text-dark font20">
+                                            Công nghệ cắt laser được ứng dụng rộng rãi trong nhiều ngành nghề khác nhau,
+                                            đặc biệt đối với ngành [...]
+                                        </p>
+                                    </div>
+    
+                                    <a href="#" title="{!!__('Read more')!!}" class="read-more text-dark font20 font-pri"> {!!__('Read more')!!} >> </a>
+                                </div>
+                            </div>
+    
+                            <div class="item mx-0">
+                                <div class="item__left">
+                                    <div class="sub--left font-pri-bold">
+                                        <div class="day"> 26 </div>
+                                        <div class="month"> May </div>
+                                        <div class="year"> 2021 </div>
+                                    </div>
+    
+                                    <div class="sub--right">
+                                        <img width="" height="" src="{{Theme::asset()->url('images/manufacturing/manu9.jpg')}}" alt="" />
+                                    </div>
+                                </div>
+    
+                                <div class="item__right">
+                                    <div class="item__right__top">
+                                        <h3 class="font30 title"> Xe tự hành AGV giải pháp vận chuyển hiệu quả</h3>
+                                        <p class="description text-dark font20">
+                                            Xe tự hành AGV là  một phương tiện dùng để vận chuyển hàng hóa và vật tư
+                                            trong nhà máy, khu công [...]
+                                        </p>
+                                    </div>
+    
+                                    <a href="#" title="{!!__('Read more')!!}" class="read-more text-dark font20 font-pri"> {!!__('Read more')!!} >> </a>
+                                </div>
+                            </div>
+    
+                            <div class="item mx-0">
+                                <div class="item__left">
+                                    <div class="sub--left font-pri-bold">
+                                        <div class="day"> 22 </div>
+                                        <div class="month"> May </div>
+                                        <div class="year"> 2021 </div>
+                                    </div>
+    
+                                    <div class="sub--right">
+                                        <img width="" height="" src="{{Theme::asset()->url('images/manufacturing/manu10.jpg')}}" alt="" />
+                                    </div>
+                                </div>
+    
+                                <div class="item__right">
+                                    <div class="item__right__top">
+                                        <h3 class="font30 title"> Nhà chế tạo Khuôn mẫu hàng đầu miền Trung </h3>
+                                        <p class="description text-dark font20">
+                                            Khuôn mẫu là “nền tảng của nền công nghiệp” Khuôn mẫu được xem là
+                                            “Nền tảng của nền công [...]
+                                        </p>
+                                    </div>
+    
+                                    <a href="#" title="{!!__('Read more')!!}" class="read-more text-dark font20 font-pri"> {!!__('Read more')!!} >> </a>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>

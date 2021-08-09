eb084ac6512cf94ab6109e509e45f4963ea7fdc0
ASUS-UX430\kHA <kha.ph@visualweber.com>
2021-08-09 18:30:37 +0700

    sass

---
+++ platform/themes/main/views/pages/business/brand-detail/section/filter-content.blade.php
@@ -0,0 +1,130 @@
+<section class="section-car-filter-content">
+    <div class="container-remake">
+        <ul class="list-car">
+            <li class="list-car__item">
+                <a class="list-car__link" href="#">
+                    <img src="{{Theme::asset()->url('images/business/brand-detail/car-item-1.png')}}" alt="" class="list-car__img">
+                    <h3 class="list-car__title font-pri font18">KIA Soluto</h3>
+                </a>
+            </li>
+            <li class="list-car__item">
+                <a class="list-car__link" href="#">
+                    <img src="{{Theme::asset()->url('images/business/brand-detail/car-item-2.png')}}" alt="" class="list-car__img">
+                    <h3 class="list-car__title font-pri font18">KIA All-New Cerato</h3>
+                </a>
+            </li>
+            <li class="list-car__item active">
+                <a class="list-car__link" href="#">
+                    <img src="{{Theme::asset()->url('images/business/brand-detail/car-item-3.png')}}" alt="" class="list-car__img">
+                    <h3 class="list-car__title font-pri font18">KIA Optima</h3>
+                </a>
+            </li>
+            <li class="list-car__item">
+                <a class="list-car__link" href="#">
+                    <img src="{{Theme::asset()->url('images/business/brand-detail/car-item-4.png')}}" alt="" class="list-car__img">
+                    <h3 class="list-car__title font-pri font18">KIA Quoris</h3>
+                </a>
+            </li>
+        </ul>
+
+        <div class="car-detail">
+            <div class="car-detail__image">
+                <div class="car-detail__background"></div>
+                <div class="car-detail__frame">
+                    <img src="{{Theme::asset()->url('images/business/brand-detail/car-detail.png')}}" alt="">
+                </div>
+                
+            </div>
+            <div class="car-detail__info">
+                <h3 class="info-title font30 font-mi-bold">KIA Optima</h3>
+                <ul class="info-equip">
+                    <li class="info-equip__item font18 font-pri">
+                        <img src="{{Theme::asset()->url('images/business/brand-detail/filter.png')}}" alt="filter icon">
+                        270 kW (367 HP)
+                    </li>
+                    <li class="info-equip__item font18 font-pri">
+                        <img src="{{Theme::asset()->url('images/business/brand-detail/filter.png')}}" alt="filter icon">
+                        Xăng
+                    </li>
+                    <li class="info-equip__item font18 font-pri">
+                        <img src="{{Theme::asset()->url('images/business/brand-detail/filter.png')}}" alt="filter icon">
+                        Tự động
+                    </li>
+                </ul>
+                <ul class="info-color">
+                    <li class="info-color__item active" style="background-color: white">
+                        
+                    </li>
+                    <li class="info-color__item" style="background-color: black">
+                        
+                    </li>
+                    <li class="info-color__item" style="background-color: #cdcdcd">
+                        
+                    </li>
+                    <li class="info-color__item" style="background-color: #cb0000">
+                        
+                    </li>
+                    <li class="info-color__item" style="background-color: #003499">
+                        
+                    </li>
+                    <li class="info-color__item" style="background-color: #235150">
+                        
+                    </li>
+                </ul>
+                <ul class="info-option">
+                    <li class="info-option__item">
+                        <h3 class="info-option-title font18 font-pri"><a href="">KIA OPTIMA 2.0 GAT LUXURY</a></h3>
+                        <p class="info-option-price font15 font-pri">Giá từ 759,000,000đ</p>
+                    </li>
+                    <li class="info-option__item">
+                        <h3 class="info-option-title font18 font-pri"><a href="">KIA OPTIMA 2.4 GAT PREMIUM</a></h3>
+                        <p class="info-option-price font15 font-pri">Giá từ 919,000,000đ</p>
+                    </li>
+                </ul>
+                <ul class="info-other">
+                    <li class="info-other__item">
+                        <a class="font18 font-pri" href="">Đăng ký lái thử</a>
+                    </li>
+                    <li class="info-other__item">
+                        <a class="font18 font-pri" href="">Xem brochure</a>
+                    </li>
+                </ul>
+                <button class="info-button font18 font-pri" type="button">Tiếp theo</button>
+                <div class="info-hotline font18 font-pri">HOTLINE - 1900 54 55 91</div>
+            </div>
+        </div>
+        <div class="related-cars">
+            <h2 class="related-cars__title font30 font-mi-bold">SẢN PHẨM ĐỀ XUẤT</h2>
+            <ul class="list-related-cars">
+                <li class="list-related-cars__item">
+                    <a class="list-related-cars__link" href="#">
+                        <img src="{{Theme::asset()->url('images/business/brand-detail/related-car-1.png')}}" alt="" class="list-related-cars__img">
+                        <h3 class="list-related-cars__title font-mi-bold font30">KIA CERATO</h3>
+                        <h4 class="list-related-cars__viewmore font-mi-bold font15">XEM CHI TIẾT <i class="fas fa-arrow-right"></i></h4>
+                    </a>
+                </li>
+                <li class="list-related-cars__item">
+                    <a class="list-related-cars__link" href="#">
+                        <img src="{{Theme::asset()->url('images/business/brand-detail/related-car-2.png')}}" alt="" class="list-related-cars__img">
+                        <h3 class="list-related-cars__title font-mi-bold font30">MAZDA 3</h3>
+                        <h4 class="list-related-cars__viewmore font-mi-bold font15">XEM CHI TIẾT <i class="fas fa-arrow-right"></i></h4>
+                    </a>
+                </li>
+                <li class="list-related-cars__item active">
+                    <a class="list-related-cars__link" href="#">
+                        <img src="{{Theme::asset()->url('images/business/brand-detail/related-car-3.png')}}" alt="" class="list-related-cars__img">
+                        <h3 class="list-related-cars__title font-mi-bold font30">MAZDA 6</h3>
+                        <h4 class="list-related-cars__viewmore font-mi-bold font15">XEM CHI TIẾT <i class="fas fa-arrow-right"></i></h4>
+                    </a>
+                </li>
+                <li class="list-related-cars__item">
+                    <a class="list-related-cars__link" href="#">
+                        <img src="{{Theme::asset()->url('images/business/brand-detail/related-car-4.png')}}" alt="" class="list-related-cars__img">
+                        <h3 class="list-related-cars__title font-mi-bold font30">BMW 320i</h3>
+                        <h4 class="list-related-cars__viewmore font-mi-bold font15">XEM CHI TIẾT <i class="fas fa-arrow-right"></i></h4>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>

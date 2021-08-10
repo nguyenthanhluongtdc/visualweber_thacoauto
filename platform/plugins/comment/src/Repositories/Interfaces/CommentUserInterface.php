9628d0d9cfaa3e1f3e3f307de189b0fce23ccc3b
Eugene <eugene@visualweber.com>
2021-08-10 17:06:58 +0700

    bootstrap

--- platform/plugins/comment/src/Repositories/Interfaces/CommentUserInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Platform\Comment\Repositories\Interfaces;
-
-use Platform\Support\Repositories\Interfaces\RepositoryInterface;
-
-interface CommentUserInterface extends RepositoryInterface
-{
-}
--- platform/themes/ripple/partials/shortcodes/all-galleries-admin-config.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="form-group">
-    <label class="control-label">{{ __('Limit') }}</label>
-    {!! Form::input('text', 'limit', Arr::get($attributes, 'limit', 8), ['class' => 'form-control']) !!}
-</div>

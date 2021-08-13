422f36d8d94cb3178944cc326ff2230fb43b94da
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-08-13 10:18:09 +0700

    sass

--- platform/themes/main/views/pages/services/service-detail.blade.php
+++ platform/themes/main/views/pages/services/service-detail.blade.php
@@ -1,18 +1,18 @@
-<section class="section-services-detail">
+<section class="section-product-detail">
     <div class="container-remake">
-        <div class="services-detail"> 
-            <h2 class="services-detail__title font-mi-bold font60">
-                CHĂM SÓC XE
+        <div class="product-detail"> 
+            <h2 class="product-detail__title font-mi-bold font60">
+                bảo hành bảo dưỡng
             </h2>
-            <img src="{{Theme::asset()->url('images/services/banner-cham-soc-xe.jpg')}}" alt="" class="services-detail__banner">
-            <h2 class="services-detail__title font-mi-bold font60">
+            <img src="{{Theme::asset()->url('images/services/banner-cham-soc-xe.jpg')}}" alt="" class="product-detail__banner">
+            <h2 class="product-detail__title font-mi-bold font60">
                 THÔNG TIN CHI TIẾT
             </h2>
-            <div class="services-detail__desc font-pri font20">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+            <div class="product-detail__desc font-pri font20">
+                Để đảm bảo chiếc xe của bạn luôn trong tình trạng hoàn hảo và an toàn, việc bảo dưỡng định kỳ được thực hiện bởi kỹ thuật viên được đào tạo chuyên nghiệp, phụ tùng chính hãng chất lượng cao, THACO có thể cung cấp dịch vụ bảo dưỡng cũng như chế độ bảo hành phù hợp cho xe bạn.
             </div>
-            <div class="services-detail__slide">
-                <div class="services-detail__slide--frame">
+            <div class="product-detail__slide">
+                <div class="product-detail__slide--frame">
                     <div class="swiper-container detail-slide">
                         <div class="swiper-wrapper">
                           <div class="swiper-slide">
@@ -30,47 +30,44 @@
                     <div class="detail-info">
                         <div class="detail-info--frame">
                             <div class="text font15 font-pri">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae
+                                Là chủ sở hữu xe THACO mới, bạn đương nhiên mong đợi các tiêu chuẩn chất lượng và độ tin cậy cao nhất từ THACO. Trong trường hợp sự cố có thể xảy ra với xe của bạn, chế độ bảo hành chính hãng sẽ bảo vệ bạn trước những chi phí sửa chữa không mong muốn.
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="services-detail__features">
-                <div class="services-detail__card">
-                    <div class="services-detail__card-image">
-                        <div class="services-detail__card-frame">
+            <div class="product-detail__features">
+                <div class="product-detail__card">
+                    <div class="product-detail__card-image">
+                        <div class="product-detail__card-frame">
                             <img src="{{Theme::asset()->url('images/services/features-cham-soc-xe.jpg')}}" alt="">
-
                         </div>
                     </div>
-                    <h3 class="services-detail__card-title font30 font-mi-bold">LOREM</h3>
-                    <div class="services-detail__card-desc font20 font-pri">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    <h3 class="product-detail__card-title font30 font-mi-bold">AN TOÀN</h3>
+                    <div class="product-detail__card-desc font20 font-pri">
+                        Các kỹ thuật viên của chung tôi tuân thủ nghiêm ngặt quy trình bảo dưỡng.
                     </div>
                 </div>
-                <div class="services-detail__card">
-                    <div class="services-detail__card-image">
-                        <div class="services-detail__card-frame">
+                <div class="product-detail__card">
+                    <div class="product-detail__card-image">
+                        <div class="product-detail__card-frame">
                             <img src="{{Theme::asset()->url('images/services/features-cham-soc-xe-2.jpg')}}" alt="">
-
                         </div>
                     </div>
-                    <h3 class="services-detail__card-title font30 font-mi-bold">LOREM</h3>
-                    <div class="services-detail__card-desc font20 font-pri">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    <h3 class="product-detail__card-title font30 font-mi-bold">TẬN TÂM</h3>
+                    <div class="product-detail__card-desc font20 font-pri">
+                        Chủ sở hữu xe THACO sẽ được tận tâm tư vấn, cung cấp thông tin chính xác và rõ ràng.
                     </div>
                 </div>
-                <div class="services-detail__card">
-                    <div class="services-detail__card-image">
-                        <div class="services-detail__card-frame">
+                <div class="product-detail__card">
+                    <div class="product-detail__card-image">
+                        <div class="product-detail__card-frame">
                             <img src="{{Theme::asset()->url('images/services/features-cham-soc-xe-3.jpg')}}" alt="">
-
                         </div>
                     </div>
-                    <h3 class="services-detail__card-title font30 font-mi-bold">LOREM</h3>
-                    <div class="services-detail__card-desc font20 font-pri">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    <h3 class="product-detail__card-title font30 font-mi-bold">CHUYÊN NGHIỆP</h3>
+                    <div class="product-detail__card-desc font20 font-pri">
+                        Quy trình làm việc là quá trình học hỏi và đúng kết từ kinh nghiệm, THACO sẽ đem lại cho bạn những trải nghiệm chuyên nghiệp nhất
                     </div>
                 </div>
             </div>

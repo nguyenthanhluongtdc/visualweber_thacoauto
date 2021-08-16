df0b9878bd9ec3a572d6fd3a8bca0c4bd7b5d8f7
ASUS-UX430\kHA <kha.ph@visualweber.com>
2021-08-16 09:00:21 +0700

    Upload tin mẫu

--- platform/themes/main/views/post.blade.php
+++ platform/themes/main/views/post.blade.php
@@ -19,8 +19,7 @@
             </div>
         </div>
         <p class="desc font20 font-mi-bold">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem ape
+            {{$post->description}}
         </p>
         <div class="post__content font-pri font25">
             @if (defined('GALLERY_MODULE_SCREEN_NAME') && !empty($galleries = gallery_meta_data($post)))

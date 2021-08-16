d7035725320345a3f663798edd08f783d92138db
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-08-16 11:47:25 +0700

    sass

--- platform/themes/main/views/media-newspapers.blade.php
+++ platform/themes/main/views/media-newspapers.blade.php
@@ -6,26 +6,33 @@
     @if (!empty($posts))
             @foreach ($posts as $post)
             <div class="item-newspaper">
-                <h5 class="title font-pri-bold font30 color-gray text-uppercase">
-                    <a href="">{{$post->name}}</a>
-                </h5>
-                <p class="desc font-pri color-gray font21">
-                    {{$post->description}}
-                </p>
-                <p class="title-docx font-cond color-gray font24">{{@get_file_name(get_field($post, 'newspaper_file'))}}</p>
-                <p class="size-dowload">
-                    <span class="left font-cond color-gray font24">{{@get_file_size(get_field($post, 'newspaper_file'))}}</span>
-                    <span class="font-pri"><a href="{{ get_object_image(get_field($post, 'newspaper_file')) }}">DOWNLOAD</a></span>
-                </p>
+                <div class="row__top">
+                    <h5 class="title font-pri-bold font30 color-gray text-uppercase">
+                        <a href="">{{$post->name}}</a>
+                    </h5>
+                    <p class="desc font-pri color-gray font21">
+                        {{$post->description}}
+                    </p>
+                </div>
                 
-                <div class="tags">
-                    <p class="name-tag font-pri-bold">Tags: </span>
-                    <div class="tag-list">
-                        @foreach ($post->tags as $tag)
-                        <p class="tag-title font-pri font13">
-                            <a href="">{{$tag->name}}</a>
-                        </p>
-                        @endforeach
+                <div class="row__bottom">
+                    <a href="#" title="" class="title-docx font-cond color-gray font24">
+                        {{@get_file_name(get_field($post, 'newspaper_file'))}}
+                    </a>
+                    <p class="size-dowload">
+                        <span class="left font-cond color-gray font24">{{@get_file_size(get_field($post, 'newspaper_file'))}}</span>
+                        <span class="font-pri"><a href="{{ get_object_image(get_field($post, 'newspaper_file')) }}">DOWNLOAD</a></span>
+                    </p>
+                    
+                    <div class="tags">
+                        <span class="name-tag font-pri-bold">Tags: </span>
+                        <div class="tag-list">
+                            @foreach ($post->tags as $tag)
+                            <p class="tag-title font-pri font13">
+                                <a href="">{{$tag->name}}</a>
+                            </p>
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
@@ -36,7 +43,7 @@
             @includeIf("theme.main::views.components.news-pagination",['paginator'=>$posts])
         @endif
 
-        {{-- <ul class="pagination font25 font-pri-bold">
+        <ul class="pagination font25 font-pri-bold">
             <li>1</li>
             <li>2</li>
             <li>3</li>
@@ -49,6 +56,6 @@
             <li>10</li>
             <li class="color">></li>
             <li class="color">>></li>
-        </ul> --}}
+        </ul>
     </div>
 </div>

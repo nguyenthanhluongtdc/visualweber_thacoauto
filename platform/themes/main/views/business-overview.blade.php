a07251ada2bc1849ec3709c57897f6e6fb98b822
ASUS-UX430\kHA <kha.ph@visualweber.com>
2021-08-17 09:58:31 +0700

    sass

--- platform/themes/main/views/business-overview.blade.php
+++ platform/themes/main/views/business-overview.blade.php
@@ -1,3 +1,127 @@
 <div class="business-overview">
     @includeIf("theme.main::views.pages.business.section.product-intro")
+    <div class="distribution">
+
+        <div class="provincial-company">
+            <div class="container-remake">
+                <h1 data-aos="fade-right" data-aos-easing="linear" data-aos-duration="1000" class="title font40">công ty tỉnh thành</h1>
+                <p data-aos="fade-left" data-aos-duration="1500" class="description font20 mt-20">THACO là doanh nghiệp hàng đầu và có quy mô lớn nhất tại Việt Nam về lĩnh vực sản xuất lắp ráp ô tô, với chuỗi giá trị từ nghiên cứu phát triển sản phẩm (R&D), sản xuất linh kiện phụ tùng, lắp ráp ô tô, đến giao nhận vận chuyển và phân phối, bán lẻ. Sản phẩm có đầy đủ các chủng loại: xe tải, xe bus, xe du lịch, xe chuyên dụng và đầy đủ phân khúc từ trung cấp đến cao cấp với doanh số và thị phần luôn dẫn đầu thị trường Việt Nam trong nhiều năm qua.</p>
+                <div data-aos="zoom-in-up" data-aos-duration="2000" class="select-list mt-40 mb-40">
+                    <form action="">
+                        <select class="ui search selection dropdown city w-100" name="" id="">
+                            <option value="">Tỉnh/Thành phố</option>
+                            <option value="1">TP.HỒ CHÍ MINH</option>
+                            <option value="2">TP.HỒ CHÍ MINH</option>
+                            <option value="3">TP.HỒ CHÍ MINH</option>
+                            <option value="4">TP.HỒ CHÍ MINH</option>
+                        </select>
+                        <button type="submit" class="font20">xác nhận</button>
+                    </form>
+                </div>
+                <div data-aos="zoom-in" data-aos-duration="2500" class="branch-wrap">
+                    <img class="branch-background-image" src="{{Theme::asset()->url('images/distribution/background.jpg')}}" alt="">
+                    <div class="branch-background-blur"></div>
+                    <div class="left">
+                        <div class="branch-overflow">
+                            <div class="branch-item mb-20">
+                                <p class="branch-name font30 mb-20">Chi Nhánh An Lạc - Công Ty CP Ô Tô Trường Hải</p>
+                                <p class="branch-address font20 mb-20">36C11, QL1A, Tân Kiên, Bình Chánh, Thành phố Hồ Chí Minh 700000</p>
+                                <div class="logo-wrap mb-20">
+                                    <div class="logo-wrap__left">
+                                        <div class="logo-item">
+                                            <img src="{{Theme::asset()->url('images/distribution/foton.png')}}" alt="">
+                                        </div>
+                                        <div class="logo-item">
+                                            <img src="{{Theme::asset()->url('images/distribution/fuso.png')}}" alt="">
+                                        </div>
+                                        <div class="logo-item">
+                                            <img src="{{Theme::asset()->url('images/distribution/frontier.png')}}" alt="">
+                                        </div>
+                                        <div class="logo-item">
+                                            <img src="{{Theme::asset()->url('images/distribution/thacobus.png')}}" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="logo-wrap__right">
+                                        <a href="/he-thong-phan-phoi-chi-tiet">Xem chi tiết <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="branch-item mb-20">
+                                <p class="branch-name font30 mb-20">Chi Nhánh An Lạc - Công Ty CP Ô Tô Trường Hải</p>
+                                <p class="branch-address font20 mb-20">36C11, QL1A, Tân Kiên, Bình Chánh, Thành phố Hồ Chí Minh 700000</p>
+                                <div class="logo-wrap mb-20">
+                                    <div class="logo-wrap__left">
+                                        <div class="logo-item">
+                                            <img src="{{Theme::asset()->url('images/distribution/foton.png')}}" alt="">
+                                        </div>
+                                        <div class="logo-item">
+                                            <img src="{{Theme::asset()->url('images/distribution/fuso.png')}}" alt="">
+                                        </div>
+                                        <div class="logo-item">
+                                            <img src="{{Theme::asset()->url('images/distribution/frontier.png')}}" alt="">
+                                        </div>
+                                        <div class="logo-item">
+                                            <img src="{{Theme::asset()->url('images/distribution/thacobus.png')}}" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="logo-wrap__right">
+                                        <a href="/he-thong-phan-phoi-chi-tiet">Xem chi tiết <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="branch-item mb-20">
+                                <p class="branch-name font30 mb-20">Chi Nhánh An Lạc - Công Ty CP Ô Tô Trường Hải</p>
+                                <p class="branch-address font20 mb-20">36C11, QL1A, Tân Kiên, Bình Chánh, Thành phố Hồ Chí Minh 700000</p>
+                                <div class="logo-wrap mb-20">
+                                    <div class="logo-wrap__left">
+                                        <div class="logo-item">
+                                            <img src="{{Theme::asset()->url('images/distribution/foton.png')}}" alt="">
+                                        </div>
+                                        <div class="logo-item">
+                                            <img src="{{Theme::asset()->url('images/distribution/fuso.png')}}" alt="">
+                                        </div>
+                                        <div class="logo-item">
+                                            <img src="{{Theme::asset()->url('images/distribution/frontier.png')}}" alt="">
+                                        </div>
+                                        <div class="logo-item">
+                                            <img src="{{Theme::asset()->url('images/distribution/thacobus.png')}}" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="logo-wrap__right">
+                                        <a href="/he-thong-phan-phoi-chi-tiet">Xem chi tiết <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="branch-item mb-20">
+                                <p class="branch-name font30 mb-20">Chi Nhánh An Lạc - Công Ty CP Ô Tô Trường Hải</p>
+                                <p class="branch-address font20 mb-20">36C11, QL1A, Tân Kiên, Bình Chánh, Thành phố Hồ Chí Minh 700000</p>
+                                <div class="logo-wrap mb-20">
+                                    <div class="logo-wrap__left">
+                                        <div class="logo-item">
+                                            <img src="{{Theme::asset()->url('images/distribution/foton.png')}}" alt="">
+                                        </div>
+                                        <div class="logo-item">
+                                            <img src="{{Theme::asset()->url('images/distribution/fuso.png')}}" alt="">
+                                        </div>
+                                        <div class="logo-item">
+                                            <img src="{{Theme::asset()->url('images/distribution/frontier.png')}}" alt="">
+                                        </div>
+                                        <div class="logo-item">
+                                            <img src="{{Theme::asset()->url('images/distribution/thacobus.png')}}" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="logo-wrap__right">
+                                        <a href="/he-thong-phan-phoi-chi-tiet">Xem chi tiết <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="right">
+                        <img src="{{Theme::asset()->url('images/distribution/mapitem.png')}}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>

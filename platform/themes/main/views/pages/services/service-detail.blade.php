2471219e711dca7eeb920aa5dcffb2326987a89a
Quốc Bình <tranaiquoc4499@gmail.com>
2021-08-19 18:55:10 +0700

    done quản lý dịch vụ

--- platform/themes/main/views/pages/services/service-detail.blade.php
+++ platform/themes/main/views/pages/services/service-detail.blade.php
@@ -1,54 +1,75 @@
 <section class="section-product-detail">
     <div class="container-remake">
-        <div class="product-detail"> 
-            <h2 class="product-detail__title font-mi-bold font60 fontmb-large">
-                bảo hành bảo dưỡng
-            </h2>
-            <img src="{{Theme::asset()->url('images/services/banner-cham-soc-xe.jpg')}}" alt="" class="product-detail__banner">
-            <h2 class="product-detail__title font-mi-bold font60 fontmb-large">
-                THÔNG TIN CHI TIẾT
-            </h2>
-            <div class="product-detail__desc font-pri font20">
-                Để đảm bảo chiếc xe của bạn luôn trong tình trạng hoàn hảo và an toàn, việc bảo dưỡng định kỳ được thực hiện bởi kỹ thuật viên được đào tạo chuyên nghiệp, phụ tùng chính hãng chất lượng cao, THACO có thể cung cấp dịch vụ bảo dưỡng cũng như chế độ bảo hành phù hợp cho xe bạn.
-            </div>
+        <div class="product-detail">
+            @if(has_field($data, 'tieu_de_service_1'))
+                <h2 class="product-detail__title font-mi-bold font60 fontmb-large">
+                    {{ get_field($data, 'tieu_de_service_1') }}
+                </h2>
+            @endif
+            @if(has_field($data, 'hinh_anh_service_1'))
+                <img src="{{ get_image_url(get_field($data, 'hinh_anh_service_1')) }}" alt="" class="product-detail__banner">
+            @endif
+            @if(has_field($data, 'mo_ta_service_1'))
+                <div class="product-detail__desc font-pri font20">
+                    {!! get_field($data, 'mo_ta_service_1') !!}
+                </div>
+            @endif
+            @if(has_field($data, 'tieu_de_service_2'))
+                <h2 class="product-detail__title font-mi-bold font60 fontmb-large">
+                    {{ get_field($data, 'tieu_de_service_2') }}
+                </h2>
+            @endif
+            @if(has_field($data, 'mo_ta_service_2'))
+                <div class="product-detail__desc font-pri font20">
+                    {!! get_field($data, 'mo_ta_service_2') !!}
+                </div>
+            @endif
             <div class="product-detail__slide">
                 <div class="product-detail__slide--frame">
                     <div class="swiper-container detail-slide">
                         <div class="swiper-wrapper">
-                          <div class="swiper-slide">
-                              <img src="{{Theme::asset()->url('images/services/slide-cham-soc-xe.jpg')}}" alt="">
-                          </div> 
-                          <div class="swiper-slide">
-                            <img src="{{Theme::asset()->url('images/services/slide-cham-soc-xe-2.jpg')}}" alt="">
-                        </div> 
-                        <div class="swiper-slide">
-                            <img src="{{Theme::asset()->url('images/services/slide-cham-soc-xe-3.jpg')}}" alt="">
-                        </div> 
+                            @if(has_field($data, 'slides'))
+                                @foreach (get_field($data, 'slides') as $item)
+                                    <div class="swiper-slide">
+                                        <img src="{{ has_sub_field($item, 'hinh_anh') ? get_image_url(get_sub_field($item, 'hinh_anh')) : "" }}" alt="">
+                                    </div>
+                                @endforeach
+                            {{-- <div class="swiper-slide">
+                                <img src="{{Theme::asset()->url('images/services/slide-cham-soc-xe-2.jpg')}}" alt="">
+                            </div>
+                            <div class="swiper-slide">
+                                <img src="{{Theme::asset()->url('images/services/slide-cham-soc-xe-3.jpg')}}" alt="">
+                            </div> --}}
+                            @endif
                         </div>
                         <div class="swiper-pagination"></div>
                     </div>
-                    <div class="detail-info">
-                        <div class="detail-info--frame">
-                            <div class="text font15 font-pri">
-                                Là chủ sở hữu xe THACO mới, bạn đương nhiên mong đợi các tiêu chuẩn chất lượng và độ tin cậy cao nhất từ THACO. Trong trường hợp sự cố có thể xảy ra với xe của bạn, chế độ bảo hành chính hãng sẽ bảo vệ bạn trước những chi phí sửa chữa không mong muốn.
+                    @if(has_field($data, 'mo_ta_slide_service_2'))
+                        <div class="detail-info">
+                            <div class="detail-info--frame">
+                                <div class="text font15 font-pri">{!! get_field($data, 'mo_ta_slide_service_2') !!}</div>
                             </div>
                         </div>
-                    </div>
+                    @endif
                 </div>
             </div>
             <div class="product-detail__features">
-                <div class="product-detail__card">
-                    <div class="product-detail__card-image">
-                        <div class="product-detail__card-frame">
-                            <img src="{{Theme::asset()->url('images/services/features-cham-soc-xe.jpg')}}" alt="">
+                @if(has_field($data, 'slogan_service_3'))
+                    @foreach (get_field($data, 'slogan_service_3') as $item)
+                        <div class="product-detail__card">
+                            <div class="product-detail__card-image">
+                                <div class="product-detail__card-frame">
+                                    <img src="{{ has_sub_field($item, 'hinh_anh') ? get_image_url(get_sub_field($item, 'hinh_anh')) : "" }}" alt="">
+                                </div>
+                            </div>
+                            <h3 class="product-detail__card-title font30 font-mi-bold">{{ has_sub_field($item, 'tieu_de') ? get_sub_field($item, 'tieu_de') : "" }}</h3>
+                            <div class="product-detail__card-desc font20 font-pri">
+                                {!! has_sub_field($item, 'mo_ta') ? get_sub_field($item, 'mo_ta') : "" !!}
+                            </div>
                         </div>
-                    </div>
-                    <h3 class="product-detail__card-title font30 font-mi-bold">AN TOÀN</h3>
-                    <div class="product-detail__card-desc font20 font-pri">
-                        Các kỹ thuật viên của chung tôi tuân thủ nghiêm ngặt quy trình bảo dưỡng.
-                    </div>
-                </div>
-                <div class="product-detail__card">
+                    @endforeach
+                @endif
+                {{-- <div class="product-detail__card">
                     <div class="product-detail__card-image">
                         <div class="product-detail__card-frame">
                             <img src="{{Theme::asset()->url('images/services/features-cham-soc-xe-2.jpg')}}" alt="">
@@ -69,7 +90,7 @@
                     <div class="product-detail__card-desc font20 font-pri">
                         Quy trình làm việc là quá trình học hỏi và đúng kết từ kinh nghiệm, THACO sẽ đem lại cho bạn những trải nghiệm chuyên nghiệp nhất
                     </div>
-                </div>
+                </div> --}}
             </div>
         </div>
     </div>

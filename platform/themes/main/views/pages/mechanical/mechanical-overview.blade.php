d98ee78cc67db0a251fcf217e3054a33b455fd15
Trúc Huỳnh <dntrh0425@gmail.com>
2021-08-20 21:16:43 +0700

    sass

--- platform/themes/main/views/pages/mechanical/mechanical-overview.blade.php
+++ platform/themes/main/views/pages/mechanical/mechanical-overview.blade.php
@@ -56,22 +56,29 @@
 <div class="section-mechanical-overview-mobile">
     <div class="container-remake">
         <div class="mechandical-overview mt-60 mb-60">
-            <h2 class="mechandical-overview__title font-pri-bold fontmb-large">CƠ KHÍ</h2>
+            @if(has_field($page, 'tittle_mechanical'))
+                 <h2 class="mechandical-overview__title font-pri-bold fontmb-large text-uppercase">{!! has_field($page,
+                    'tittle_mechanical') !!}</h2>
+            @endif
+           
+            @if(has_field($page, 'description_mechanical'))
             <div class="mechandical-overview__desc font-pri mt-40 mb-40 fontmb-little">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                {!! has_field($page, 'description_mechanical') !!}
                  
             </div>
+            @endif
+          
+           
             <div class="mechandical-overview__boxfield row">
-                
+                @if(has_field($page, 'repeater_module_mechanical'))
+                @foreach(has_field($page, 'repeater_module_mechanical') as $row)
                <div class="boxfield-item col-md-5 col-sm-5 col-5 p-0">
-                   <img src="{{ Theme::asset()->url('images/mechandical/thep-giacong.png') }}" alt="">
-                   <p class="font40 font-pri-bold name fontmb-medium">THÉP VÀ DỊCH VỤ
-                    GIA CÔNG THÉP</p>
+                <img src="{{ has_sub_field($row, 'symbol') }}" alt="{!! has_sub_field($row, 'symbol') !!}" alt="">
+                   <p class="font40 font-pri-bold name fontmb-medium">{!! has_sub_field($row, 'name') !!}</p>
                </div>
-               <div class="boxfield-item col-md-5 col-sm-5 col-5">
+               @endforeach
+               @endif
+               {{-- <div class="boxfield-item col-md-5 col-sm-5 col-5">
                     <img src="{{ Theme::asset()->url('images/mechandical/giacong-khuonmau.png') }}" alt="">
                     
                     <p class="font40 font-pri-bold name fontmb-medium">GIA CÔNG CƠ KHÍ <br>
@@ -97,7 +104,7 @@
                     <p class="font40 font-pri-bold name fontmb-medium">CƠ KHÍ NÔNG
                         LÂM NGHIỆP</p>
                 </div>
-                
+                 --}}
                
             </div>
             
--- platform/themes/main/views/pages/production-assemble/production-assemble-overview.blade.php
+++ platform/themes/main/views/pages/production-assemble/production-assemble-overview.blade.php
@@ -2,58 +2,67 @@
 <div class="section-production-assemble-overview">
     <div class="container-remake">
         <div class="mechandical-overview mt-60 mb-60">
-            <h2 class="mechandical-overview__title font60 font-pri-bold">SẢN XUẤT LẮP RÁP</h2>
+          
+            @if(has_field($page, 'tittle_production_assemble'))
+            <h2 class="mechandical-overview__title font60 font-pri-bold text-uppercase">{!! has_field($page,
+                'tittle_production_assemble') !!}</h2>
+            @endif
+            @if(has_field($page, 'description_production_assemble'))
             <div class="mechandical-overview__desc font20 font-pri mt-40 mb-40">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                {!! has_field($page, 'description_production_assemble') !!}
                   
             </div>
+            @endif
             <div class="mechandical-overview__boxfield">
+
+                @if(has_field($page, 'repeater_module_production_assemble'))
+                @foreach(has_field($page, 'repeater_module_production_assemble') as $row)
                <div class="boxfield-item item">
-                <img src="{{ Theme::asset()->url('images/manufacturing/assemble/kia.png') }}" alt="">
+                <img src="{{ has_sub_field($row, 'symbol') }}" alt="{!! has_sub_field($row, 'symbol') !!}" alt="">
                     <div class="text">
-                        <p class="font24 font-pri-bold name">XE KIA</p>
+                        <p class="font24 font-pri-bold name">{!! has_sub_field($row, 'name') !!}</p>
                     </div>
                   
                </div>
+               @endforeach
+               @endif
+{{-- 
                <div class="boxfield-item item">
                 <img src="{{ Theme::asset()->url('images/manufacturing/assemble/logo-mazda2.png') }}" alt="">
                 <div class="text">
                     <p class="font24 font-pri-bold name">XE MAZDA</p>
                 </div>
-                    {{-- <p class="font24 font-pri-bold name">XE MAZDA</p> --}}
+                  
                 </div>
                 <div class="boxfield-item item">
                     <img src="{{ Theme::asset()->url('images/manufacturing/assemble/peugeot2.png') }}" alt="">
-                    {{-- <p class="font24 font-pri-bold name">XE PEUGEOT</p> --}}
+                  
                     <div class="text">
                         <p class="font24 font-pri-bold name">XE PEUGEOT</p>
                     </div>
                 </div>
                 <div class="boxfield-item item">
                     <img src="{{ Theme::asset()->url('images/manufacturing/assemble/xebus.png') }}" alt="">
-                    {{-- <p class="font24 font-pri-bold name">XE BUS</p> --}}
+                   
                     <div class="text">
                         <p class="font24 font-pri-bold name">XE BUS</p>
                     </div>
                 </div>
                 <div class="boxfield-item item">
                     <img src="{{ Theme::asset()->url('images/manufacturing/assemble/xetai.png') }}" alt="">
-                    {{-- <p class="font24 font-pri-bold name">XE TẢI</p> --}}
+                   
                     <div class="text">
                         <p class="font24 font-pri-bold name">XE TẢI</p>
                     </div>
                 </div>
                 <div class="boxfield-item item">
                     <img src="{{ Theme::asset()->url('images/manufacturing/assemble/chuyendung.png') }}" alt="">
-                    {{-- <p class="font24 font-pri-bold name">XE CHUYÊN DỤNG</p> --}}
+                  
                     <div class="text">
                         <p class="font24 font-pri-bold name">XE CHUYÊN DỤNG</p>
                     </div>
                 </div>
-                
+                 --}}
                
             </div>
         </div>

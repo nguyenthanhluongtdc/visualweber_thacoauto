00a7c1195a4f1096fa7bc13e3b7a948a28ea721d
Trúc Huỳnh <dntrh0425@gmail.com>
2021-08-20 19:08:37 +0700

    sass

--- platform/themes/main/views/pages/mechanical/mechanical-overview.blade.php
+++ platform/themes/main/views/pages/mechanical/mechanical-overview.blade.php
@@ -1,21 +1,25 @@
 <div class="section-mechanical-overview">
     <div class="container-remake">
         <div class="mechandical-overview mt-60 mb-60">
-            <h2 class="mechandical-overview__title font60 font-pri-bold">CƠ KHÍ</h2>
+            @if(has_field($page, 'tittle_mechanical'))
+            <h2 class="mechandical-overview__title font60 font-pri-bold text-uppercase">{!! has_field($page,
+                'tittle_mechanical') !!}</h2>
+            @endif
+            @if(has_field($page, 'description_mechanical'))
             <div class="mechandical-overview__desc font20 font-pri mt-40 mb-40">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                 
+                {!! has_field($page, 'description_mechanical') !!}
             </div>
+            @endif
             <div class="mechandical-overview__boxfield pl-3">
+                @if(has_field($page, 'repeater_module_mechanical'))
+                @foreach(has_field($page, 'repeater_module_mechanical') as $row)
                <div class="boxfield-item">
-                   <img src="{{ Theme::asset()->url('images/mechandical/thep-giacong.png') }}" alt="">
-                   <p class="font24 font-pri-bold name">THÉP VÀ DỊCH VỤ
-                    GIA CÔNG THÉP</p>
+                   <img src="{{ has_sub_field($row, 'symbol') }}" alt="{!! has_sub_field($row, 'symbol') !!}" alt="">
+                   <p class="font24 font-pri-bold name text-uppercase">{!! has_sub_field($row, 'name') !!}</p>
                </div>
-               <div class="boxfield-item">
+               @endforeach
+               @endif
+               {{-- <div class="boxfield-item">
                     <img src="{{ Theme::asset()->url('images/mechandical/giacong-khuonmau.png') }}" alt="">
                     
                     <p class="font24 font-pri-bold name">GIA CÔNG CƠ KHÍ
@@ -41,7 +45,7 @@
                     <p class="font24 font-pri-bold name">CƠ KHÍ NÔNG
                         LÂM NGHIỆP</p>
                 </div>
-                
+                 --}}
                
             </div>
             

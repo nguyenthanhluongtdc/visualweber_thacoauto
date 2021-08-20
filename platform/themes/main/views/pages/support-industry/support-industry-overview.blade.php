0cc99bfc315761bb8e88395cdd5bd06565542f15
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-08-20 12:25:42 +0700

    sass

--- platform/themes/main/views/pages/support-industry/support-industry-overview.blade.php
+++ platform/themes/main/views/pages/support-industry/support-industry-overview.blade.php
@@ -1,76 +1,68 @@
-
 <div class="section-support-industry-overview">
     <div class="container-remake">
         <div class="mechandical-overview mt-60 mb-60">
-            <h2 class="mechandical-overview__title font60 font-pri-bold">CÔNG NGHIỆP HỖ TRỢ</h2>
+            @if(has_field($page, 'name_module_overview_sindustry'))
+            <h2 class="mechandical-overview__title font60 font-pri-bold"> {!! has_field($page,
+                'name_module_overview_sindustry') !!} </h2>
+            @endif
+
+            @if(has_field($page, 'description_module_overview_sindustry'))
             <div class="mechandical-overview__desc font20 font-pri mt-40 mb-40">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                 
+                {!! has_field($page, 'description_module_overview_sindustry') !!}
             </div>
+            @endif
+
             <div class="mechandical-overview__boxfield">
-               <div class="boxfield-item" >
-                <img src="{{ Theme::asset()->url('images/support-industry/linhkien-oto.png') }}" alt="">
-                   <p class="font24 font-pri-bold name">LINH KIỆN Ô TÔ</p>
-               </div>
-               <div class="boxfield-item">
-                <img src="{{ Theme::asset()->url('images/support-industry/phukienoto.png') }}" alt="">
-                    
-                    <p class="font24 font-pri-bold name">PHỤ KIỆN Ô TÔ</p>
-                </div>
-                <div class="boxfield-item">
-                    <img src="{{ Theme::asset()->url('images/support-industry/linhkien-xemay.png') }}" alt="">
-                    <p class="font24 font-pri-bold name">LINH KIỆN XE MÁY<br>
-                        Ô TÔ</p>
-                </div>
-                <div class="boxfield-item">
-                    <img src="{{ Theme::asset()->url('images/support-industry/sanpham-phutro.png') }}" alt="">
-                    <p class="font24 font-pri-bold name">SẢN PHẨM PHỤ TRỢ</p>
-                </div>
-                
-               
+                @if(has_field($page, 'repeater_module_overview_sindustry'))
+                    @foreach(has_field($page, 'repeater_module_overview_sindustry') as $row)
+                        <div class="boxfield-item">
+                            <div class="bg-color h-100 w-100">
+                                <img src="{{ has_sub_field($row, 'image') }}" alt="{!! has_sub_field($row, 'name') !!}">
+                                <p class="font24 font-pri-bold name"> {!! has_sub_field($row, 'name') !!} </p>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
             </div>
         </div>
     </div>
 </div>
-<div class="section-support-industry-overview-mobile">
+{{-- <div class="section-support-industry-overview-mobile">
     <div class="container-remake">
         <div class="mechandical-overview mt-60 mb-60">
-            <h2 class="mechandical-overview__title fontmb-large font-pri-bold">CÔNG NGHIỆP HỖ TRỢ</h2>
-            <div class="mechandical-overview__desc fontmb-little font-pri mt-40 mb-40">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                 
-            </div>
+            @if(has_field($page, 'name_module_overview_sindustry'))
+                <h2 class="mechandical-overview__title fontmb-large font-pri-bold">
+                    {!! has_field($page, 'name_module_overview_sindustry') !!}
+                </h2>
+            @endif
+
+            @if(has_field($page, 'description_module_overview_sindustry'))
+                <div class="mechandical-overview__desc fontmb-little font-pri mt-40 mb-40">
+                    {!! has_field($page, 'description_module_overview_sindustry') !!}n 
+                </div>
+            @endif
+            
             <div class="mechandical-overview__boxfield row">
                 
                <div class="boxfield-item col-md-5 col-sm-5 col-5 p-0">
                    <img src="{{ Theme::asset()->url('images/support-industry/linhkien-oto.png') }}" alt="">
-                   <p class="fontmb-medium font-pri-bold name">LINH KIỆN Ô TÔ</p>
-               </div>
-               <div class="boxfield-item col-md-5 col-sm-5 col-5">
-                    <img src="{{ Theme::asset()->url('images/support-industry/phukienoto.png') }}" alt="">
-                    
-                    <p class="fontmb-medium font-pri-bold name">PHỤ KIỆN Ô TÔ</p>
-                </div>
-                <div class="boxfield-item col-md-5 col-sm-5 col-5">
-                    <img src="{{ Theme::asset()->url('images/support-industry/linhkien-xemay.png') }}" alt="">
-                    <p class="fontmb-medium font-pri-bold name">LINH KIỆN XE MÁY<br>
-                        Ô TÔ</p>
-                </div>
-                <div class="boxfield-item col-md-5 col-sm-5 col-5">
-                    <img src="{{ Theme::asset()->url('images/support-industry/sanpham-phutro.png') }}" alt="">
-                    <p class="fontmb-medium font-pri-bold name">SẢN PHẨM PHỤ TRỢ</p>
-                </div>
-               
-                
-               
-            </div>
-            
-        </div>
-    </div>
+<p class="fontmb-medium font-pri-bold name">LINH KIỆN Ô TÔ</p>
 </div>
+<div class="boxfield-item col-md-5 col-sm-5 col-5">
+    <img src="{{ Theme::asset()->url('images/support-industry/phukienoto.png') }}" alt="">
+
+    <p class="fontmb-medium font-pri-bold name">PHỤ KIỆN Ô TÔ</p>
+</div>
+<div class="boxfield-item col-md-5 col-sm-5 col-5">
+    <img src="{{ Theme::asset()->url('images/support-industry/linhkien-xemay.png') }}" alt="">
+    <p class="fontmb-medium font-pri-bold name">LINH KIỆN XE MÁY<br>
+        Ô TÔ</p>
+</div>
+<div class="boxfield-item col-md-5 col-sm-5 col-5">
+    <img src="{{ Theme::asset()->url('images/support-industry/sanpham-phutro.png') }}" alt="">
+    <p class="fontmb-medium font-pri-bold name">SẢN PHẨM PHỤ TRỢ</p>
+</div>
+</div>
+</div>
+</div>
+</div> --}}
--- platform/themes/main/views/support-industry.blade.php
+++ platform/themes/main/views/support-industry.blade.php
@@ -4,5 +4,4 @@
     @includeIf("theme.main::views.pages.mechanical.mechanical-news")
     @includeIf("theme.main::views.pages.mechanical.mechanical-news-mobile")
     @includeIf("theme.main::views.pages.mechanical.mechanical-contact")
-
 </div>

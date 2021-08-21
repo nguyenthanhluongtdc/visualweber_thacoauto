3c75f4576549d0bd0a17ccf37000dcaefa88ee40
Trúc Huỳnh <dntrh0425@gmail.com>
2021-08-21 19:50:03 +0700

    sass

--- platform/themes/main/views/pages/mechanical/mechanical-overview.blade.php
+++ platform/themes/main/views/pages/mechanical/mechanical-overview.blade.php
@@ -3,12 +3,12 @@
     <div class="container-remake">
         <div class="mechandical-overview mt-60 mb-60">
             @if(has_field($page, 'tittle_mechanical'))
-                <h2 class="mechandical-overview__title font60 font-pri-bold"> {!! has_field($page,
+                <h2 class="mechandical-overview__title font60 font-pri-bold fontmb-large text-uppercase"> {!! has_field($page,
                 'tittle_mechanical') !!} </h2>
             @endif
 
             @if(has_field($page, 'description_mechanical'))
-                <div class="mechandical-overview__desc font20 font-pri mt-40 mb-40">
+                <div class="mechandical-overview__desc font20 font-pri mt-40 mb-40 fontmb-small">
                     {!! has_field($page, 'description_mechanical') !!}
                 </div>
             @endif
@@ -20,7 +20,7 @@
                             <div class="content">
                                 <div class="center">
                                     <img src="{{Storage::disk('public')->exists(has_sub_field($row, 'symbol')) ? get_image_url(has_sub_field($row, 'symbol')) : RvMedia::getDefaultImage()}}" alt="">
-                                    <p class="font24 font-pri-bold name"> {!! has_sub_field($row, 'name') !!} </p>
+                                    <p class="font24 font-pri-bold name fontmb-middle text-uppercase"> {!! has_sub_field($row, 'name') !!} </p>
                                 </div>
                             </div>
                         </div>

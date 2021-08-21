e9b06bd5b64760e711aa8fa8d0eee1f57068cbe5
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-08-21 18:58:13 +0700

    sass

--- platform/themes/main/views/pages/mechanical/mechanical-overview.blade.php
+++ platform/themes/main/views/pages/mechanical/mechanical-overview.blade.php
@@ -1,113 +1,32 @@
-<div class="section-mechanical-overview">
-    <div class="container-remake">
-        <div class="mechandical-overview mt-60 mb-60">
-            @if(has_field($page, 'tittle_mechanical'))
-            <h2 class="mechandical-overview__title font60 font-pri-bold text-uppercase">{!! has_field($page,
-                'tittle_mechanical') !!}</h2>
-            @endif
-            @if(has_field($page, 'description_mechanical'))
-            <div class="mechandical-overview__desc font20 font-pri mt-40 mb-40">
-                {!! has_field($page, 'description_mechanical') !!}
-            </div>
-            @endif
-            <div class="mechandical-overview__boxfield pl-3">
-                @if(has_field($page, 'repeater_module_mechanical'))
-                @foreach(has_field($page, 'repeater_module_mechanical') as $row)
-               <div class="boxfield-item">
-                   <img src="{{get_image_url(has_sub_field($row, 'symbol'))}}" alt="">
-                   <p class="font24 font-pri-bold name text-uppercase">{!! has_sub_field($row, 'name') !!}</p>
-               </div>
-               @endforeach
-               @endif
-               {{-- <div class="boxfield-item">
-                    <img src="{{ Theme::asset()->url('images/mechandical/giacong-khuonmau.png') }}" alt="">
-                    
-                    <p class="font24 font-pri-bold name">GIA CÔNG CƠ KHÍ
-                        KHUÔN MẪU</p>
-                </div>
-                <div class="boxfield-item">
-                    <img src="{{ Theme::asset()->url('images/mechandical/cokhi-oto.png') }}" alt="">
-                    <p class="font24 font-pri-bold name">CƠ KHÍ<br>
-                        Ô TÔ</p>
-                </div>
-                <div class="boxfield-item">
-                    <img src="{{ Theme::asset()->url('images/mechandical/cokhi-xaydung.png') }}" alt="">
-                    <p class="font24 font-pri-bold name">CƠ KHÍ <br>
-                        XÂY DỰNG</p>
-                </div>
-                <div class="boxfield-item">
-                    <img src="{{ Theme::asset()->url('images/mechandical/thietbi-congnghiep.png') }}" alt="">
-                    <p class="font24 font-pri-bold name">THIẾT BỊ<br>
-                        CÔNG NGHIỆP</p>
-                </div>
-                <div class="boxfield-item">
-                    <img src="{{ Theme::asset()->url('images/mechandical/lamnghiep.png') }}" alt="">
-                    <p class="font24 font-pri-bold name">CƠ KHÍ NÔNG
-                        LÂM NGHIỆP</p>
-                </div>
-                 --}}
-               
-            </div>
-            
-        </div>
-    </div>
-</div>
 
-<div class="section-mechanical-overview-mobile">
+<div class="section-support-industry-overview">
     <div class="container-remake">
         <div class="mechandical-overview mt-60 mb-60">
             @if(has_field($page, 'tittle_mechanical'))
-                 <h2 class="mechandical-overview__title font-pri-bold fontmb-large text-uppercase">{!! has_field($page,
-                    'tittle_mechanical') !!}</h2>
+                <h2 class="mechandical-overview__title font60 font-pri-bold"> {!! has_field($page,
+                'tittle_mechanical') !!} </h2>
             @endif
-           
+
             @if(has_field($page, 'description_mechanical'))
-            <div class="mechandical-overview__desc font-pri mt-40 mb-40 fontmb-little">
-                {!! has_field($page, 'description_mechanical') !!}
-                 
-            </div>
+                <div class="mechandical-overview__desc font20 font-pri mt-40 mb-40">
+                    {!! has_field($page, 'description_mechanical') !!}
+                </div>
             @endif
-          
-           
-            <div class="mechandical-overview__boxfield row">
+
+            <div class="mechandical-overview__boxfield">
                 @if(has_field($page, 'repeater_module_mechanical'))
-                @foreach(has_field($page, 'repeater_module_mechanical') as $row)
-               <div class="boxfield-item col-md-5 col-sm-5 col-5 p-0">
-                <img src="{{ has_sub_field($row, 'symbol') }}" alt="{!! has_sub_field($row, 'symbol') !!}" alt="">
-                   <p class="font40 font-pri-bold name fontmb-medium">{!! has_sub_field($row, 'name') !!}</p>
-               </div>
-               @endforeach
-               @endif
-               {{-- <div class="boxfield-item col-md-5 col-sm-5 col-5">
-                    <img src="{{ Theme::asset()->url('images/mechandical/giacong-khuonmau.png') }}" alt="">
-                    
-                    <p class="font40 font-pri-bold name fontmb-medium">GIA CÔNG CƠ KHÍ <br>
-                        KHUÔN MẪU</p>
-                </div>
-                <div class="boxfield-item col-md-5 col-sm-5 col-5">
-                    <img src="{{ Theme::asset()->url('images/mechandical/cokhi-oto.png') }}" alt="">
-                    <p class="font40 font-pri-bold name fontmb-medium">CƠ KHÍ
-                        Ô TÔ</p>
-                </div>
-                <div class="boxfield-item col-md-5 col-sm-5 col-5">
-                    <img src="{{ Theme::asset()->url('images/mechandical/cokhi-xaydung.png') }}" alt="">
-                    <p class="font40 font-pri-bold name fontmb-medium">CƠ KHÍ
-                        XÂY DỰNG</p>
-                </div>
-                <div class="boxfield-item col-md-5 col-sm-5 col-5">
-                    <img src="{{ Theme::asset()->url('images/mechandical/thietbi-congnghiep.png') }}" alt="">
-                    <p class="font40 font-pri-bold name fontmb-medium">THIẾT BỊ
-                        CÔNG NGHIỆP</p>
-                </div>
-                <div class="boxfield-item col-md-5 col-sm-5 col-5">
-                    <img src="{{ Theme::asset()->url('images/mechandical/lamnghiep.png') }}" alt="">
-                    <p class="font40 font-pri-bold name fontmb-medium">CƠ KHÍ NÔNG
-                        LÂM NGHIỆP</p>
-                </div>
-                 --}}
-               
+                    @foreach(has_field($page, 'repeater_module_mechanical') as $row)
+                        <div class="boxfield-item item">
+                            <div class="content">
+                                <div class="center">
+                                    <img src="{{Storage::disk('public')->exists(has_sub_field($row, 'symbol')) ? get_image_url(has_sub_field($row, 'symbol')) : RvMedia::getDefaultImage()}}" alt="">
+                                    <p class="font24 font-pri-bold name"> {!! has_sub_field($row, 'name') !!} </p>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
             </div>
-            
         </div>
     </div>
 </div>
--- platform/themes/main/views/pages/support-industry/support-industry-tab.blade.php
+++ platform/themes/main/views/pages/support-industry/support-industry-tab.blade.php
@@ -5,13 +5,13 @@
         style="background-image:url('{{ Theme::asset()->url('images/mechandical/bg-tab2.png') }}')">
         <div class="container-remake mechanical-tab__content" data-aos="fade-up" data-aos-duration="1500"
             data-aos-easing="ease-in-out" data-aos-delay="50">
-            <div class="container py-5">
+            <div class="container py-lg-5 py-4">
                 <div class="row">
                     {!!
                         Menu::renderMenuLocation($name_menu, [
                             'options' => [],
                             'theme' => true,
-                            'view' => 'tabs_manufacturing',
+                            'view' => 'tabs_business',
                         ])
                     !!}
                 </div>

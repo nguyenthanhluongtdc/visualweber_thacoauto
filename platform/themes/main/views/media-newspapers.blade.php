bc893b99e2521c745178a2b838b35bd7179b1a4f
Trúc Huỳnh <dntrh0425@gmail.com>
2021-08-24 15:56:17 +0700

    sass

--- platform/themes/main/views/media-newspapers.blade.php
+++ platform/themes/main/views/media-newspapers.blade.php
@@ -1,12 +1,12 @@
 {!! do_shortcode('[filter-media][/filter-media]') !!}
 <div class="list-newspapers container-remake">
     @php
-        $posts = get_posts_by_category(18, 3);
+        $posts = get_posts_by_category($category->id ?? 16, 3);
     @endphp
     @if (!empty($posts))
             @foreach ($posts as $post)
             <div class="item-newspaper">
-                <div class="row__top"> 
+                <div class="row__top">
                     <h5 class="title font-pri-bold font30 color-gray text-uppercase ">
                         <a href="">{{$post->name}}</a>
                     </h5>
@@ -14,21 +14,21 @@
                         {{$post->description}}
                     </p>
                 </div>
-                
+
                 <div class="row__bottom">
                     <a href="#" title="" class="title-docx font-cond color-gray font24">
                         {{@get_file_name(get_field($post, 'newspaper_file'))}}
                     </a>
                     <p class="size-dowload">
                         <span class="left font-cond color-gray font24">{{@get_file_size(get_field($post, 'newspaper_file'))}}</span>
-                        <span class="font-pri"><a href="{{ get_object_image(get_field($post, 'newspaper_file')) }}">DOWNLOAD</a></span>
+                        <span class="font-pri"><a href="{{ get_object_image(get_field($post, 'newspaper_file')) }}">{{ __("DOWNLOAD") }}</a></span>
                     </p>
-                    
+
                     <div class="tags">
                         <div class="tag-name">
-                            <span class="name-tag font-pri-bold font13">Tags: </span>
+                            <span class="name-tag font-pri-bold font13">{{ __("Tags") }}: </span>
                         </div>
-                       
+
                         <div class="tag-list">
                             @foreach ($post->tags as $tag)
                             <p class="tag-title font-pri font13 mb-3">
@@ -41,7 +41,7 @@
             </div>
             @endforeach
         @endif
-    
+
 </div>
 <div class="container-remake">
     {{ $posts->links('vendor.pagination.custom') }}

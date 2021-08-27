683857124a79baeb435b2ceccb21803d5e6afa78
PC02 <pc02@visualweber.com>
2021-08-27 14:59:44 +0700

    no message

--- public/themes/main/js/map.js
+++ public/themes/main/js/map.js
@@ -206,7 +206,7 @@ const Distribution = {
                                 + popupDetailWrap +
                                 `</div>
                                 <div class="branch-footer">
-                                    <a href="#"><button>${window.__distribution ? window.__distribution.readmore : ''}</button></a>
+                                    <a href="${popup_info.seemore}"><button>${window.__distribution ? window.__distribution.readmore : ''}</button></a>
                                 </div>
                             </div>`
         

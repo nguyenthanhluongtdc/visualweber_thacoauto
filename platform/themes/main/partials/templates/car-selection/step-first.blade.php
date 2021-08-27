27744e24928a2bded57b7e5d5206c46c5b446d31
Quốc Bình <tranaiquoc4499@gmail.com>
2021-08-28 00:28:23 +0700

    -

--- platform/themes/main/partials/templates/car-selection/step-first.blade.php
+++ platform/themes/main/partials/templates/car-selection/step-first.blade.php
@@ -8,7 +8,7 @@
                 <ul class="info-equip">
                     <li class="info-equip__item font15 font-pri">
                         <img src="{{Theme::asset()->url('images/business/brand-detail/chon-xe-1.png')}}" alt="filter icon">
-                        <span>{{ number_format($car->horse_power * 0.7457, 2, '.', ',') }} kW ({{ $car->horse_power }} HP)</span>
+                        <span>{{ number_format($car->horse_power * 0.7457, 2, '.', ',') }} {{ __("kW") }} ({{ $car->horse_power }} {{ __("HP") }})</span>
                     </li>
                     <li class="info-equip__item font15 font-pri">
                         <img src="{{Theme::asset()->url('images/business/brand-detail/chon-xe-2.png')}}" alt="filter icon">
@@ -23,158 +23,37 @@
                     <img src="{{Theme::asset()->url('images/business/brand-detail/360.png')}}" alt="">
                 </div>
             </div>
+
             <div class="car-selection-content__right  col-sm-12 col-md-12">
                 <div class="car-version">
                     <div id="car-version-select" class="car-version__select font15 font-pri">
-                        <h3 class="font15 font-pri mb-0">Chọn phiên bản xe</h3>
-                        <i class="fas fa-chevron-down font12"></i>
+                        @if($car->childrents)
+                            <h3 class="font15 font-pri mb-0">{{ __("Chọn phiên bản xe") }}</h3>
+                            <i class="fas fa-chevron-down font12"></i>
+                        @endif
                     </div>
 
                     <a href="" class="car-version__viewdetail font15 font-pri">Xem chi tiết phiên bản</a>
                 </div>
-                <ul id="car-version-list" class="car-version__list active">
-                    <li class="car-version__item">
-                        <a href="">
-                            <div class="car-version__title font15 font-pri">KIA OPTIMA 2.0 GAT LUXURY</div>
-                            <div class="car-version__price font15 font-pri">Giá từ 759,000,000đ</div>
-                        </a>
-                    </li>
-                    <li class="car-version__item">
-                        <a href="">
-                            <div class="car-version__title font15 font-pri">KIA OPTIMA 2.4 GAT PREMIUM</div>
-                            <div class="car-version__price font15 font-pri">Giá từ 919,000,000đ</div>
-                        </a>
-                    </li>
-                </ul>
-                <div class="gray-line"></div>
-                <div class="select-color">
-                    <h3 class="select-color__title font15 font-pri">Lựa chọn màu</h3>
-                    <ul class="info-color">
-                        <li class="info-color__item active" style="background-color: white">
-
+                @if($car->childrents)
+                    <ul id="car-version-list" class="car-version__list active">
+                        <li class="car-version__item">
+                            <a href="">
+                                <div class="car-version__title font15 font-pri">KIA OPTIMA 2.0 GAT LUXURY</div>
+                                <div class="car-version__price font15 font-pri">Giá từ 759,000,000đ</div>
+                            </a>
                         </li>
-                        <li class="info-color__item" style="background-color: black">
-
-                        </li>
-                        <li class="info-color__item" style="background-color: #cdcdcd">
-
-                        </li>
-                        <li class="info-color__item" style="background-color: #cb0000">
-
-                        </li>
-                        <li class="info-color__item" style="background-color: #003499">
-
-                        </li>
-                        <li class="info-color__item" style="background-color: #235150">
-
+                        <li class="car-version__item">
+                            <a href="">
+                                <div class="car-version__title font15 font-pri">KIA OPTIMA 2.4 GAT PREMIUM</div>
+                                <div class="car-version__price font15 font-pri">Giá từ 919,000,000đ</div>
+                            </a>
                         </li>
                     </ul>
-                </div>
-                <div class="select-equip">
-                    <h3 class="select-equip__title font15 font-pri">Phụ kiện</h3>
-                    <div class="select-equip__list">
-                        <div class="select-equip__item active">
-                            <div class="frame">
-                                <img src="{{Theme::asset()->url('images/business/brand-detail/phu-kien-1.jpg')}}" alt="">
-                            </div>
-                            <div class="checkbox-frame">
-                                <div class="cframe">
-                                    <input class="checkbox" type="checkbox">
-                                    <span class="checkmark"></span>
-                                </div>
-                                <label class="font-pri font12" for="">Lorem ipsum <span>2.000.000đ</span> </label>
-                            </div>
-                        </div>
-                        <div class="select-equip__item">
-                            <div class="frame">
-                                <img src="{{Theme::asset()->url('images/business/brand-detail/phu-kien-2.jpg')}}" alt="">
-                            </div>
-                            <div class="checkbox-frame">
-                                <div class="cframe">
-                                    <input class="checkbox" type="checkbox">
-                                    <span class="checkmark"></span>
-                                </div>
-                                <label class="font-pri font12" for="">Lorem ipsum <span>2.000.000đ</span> </label>
-                            </div>
-                        </div>
-                        <div class="select-equip__item">
-                            <div class="frame">
-                                <img src="{{Theme::asset()->url('images/business/brand-detail/phu-kien-3.jpg')}}" alt="">
-                            </div>
-                            <div class="checkbox-frame">
-                                <div class="cframe">
-                                    <input class="checkbox" type="checkbox">
-                                    <span class="checkmark"></span>
-                                </div>
-                                <label class="font-pri font12" for="">Lorem ipsum <span>2.000.000đ</span> </label>
-                            </div>
-                        </div>
-                        <div class="select-equip__item">
-                            <div class="frame">
-                                <img src="{{Theme::asset()->url('images/business/brand-detail/phu-kien-4.jpg')}}" alt="">
-                            </div>
-                            <div class="checkbox-frame">
-                                <div class="cframe">
-                                    <input class="checkbox" type="checkbox">
-                                    <span class="checkmark"></span>
-                                </div>
-                                <label class="font-pri font12" for="">Lorem ipsum <span>2.000.000đ</span> </label>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="select-equip">
-                    <h3 class="select-equip__title font15 font-pri">Trang bị thêm</h3>
-                    <div class="select-equip__list">
-                        <div class="select-equip__item active">
-                            <div class="frame">
-                                <img src="{{Theme::asset()->url('images/business/brand-detail/phu-kien-5.jpg')}}" alt="">
-                            </div>
-                            <div class="checkbox-frame">
-                                <div class="cframe">
-                                    <input class="checkbox" type="checkbox">
-                                    <span class="checkmark"></span>
-                                </div>
-                                <label class="font-pri font12" for="">Lorem ipsum <span>2.000.000đ</span> </label>
-                            </div>
-                        </div>
-                        <div class="select-equip__item">
-                            <div class="frame">
-                                <img src="{{Theme::asset()->url('images/business/brand-detail/phu-kien-6.jpg')}}" alt="">
-                            </div>
-                            <div class="checkbox-frame">
-                                <div class="cframe">
-                                    <input class="checkbox" type="checkbox">
-                                    <span class="checkmark"></span>
-                                </div>
-                                <label class="font-pri font12" for="">Lorem ipsum <span>2.000.000đ</span> </label>
-                            </div>
-                        </div>
-                        <div class="select-equip__item">
-                            <div class="frame">
-                                <img src="{{Theme::asset()->url('images/business/brand-detail/phu-kien-7.jpg')}}" alt="">
-                            </div>
-                            <div class="checkbox-frame">
-                                <div class="cframe">
-                                    <input class="checkbox" type="checkbox">
-                                    <span class="checkmark"></span>
-                                </div>
-                                <label class="font-pri font12" for="">Lorem ipsum <span>2.000.000đ</span> </label>
-                            </div>
-                        </div>
-                        <div class="select-equip__item">
-                            <div class="frame">
-                                <img src="{{Theme::asset()->url('images/business/brand-detail/phu-kien-8.jpg')}}" alt="">
-                            </div>
-                            <div class="checkbox-frame">
-                                <div class="cframe">
-                                    <input class="checkbox" type="checkbox">
-                                    <span class="checkmark"></span>
-                                </div>
-                                <label class="font-pri font12" for="">Lorem ipsum <span>2.000.000đ</span> </label>
-                            </div>
-                        </div>
-                    </div>
+                @endif
+                <div class="gray-line"></div>
+                <div class="option__car">
+                    {!! Theme::partial('templates.car-selection.options', ['car' => $car]) !!}
                 </div>
                 <a class="select-button font18 font-pri fontmb-small d-inline-block"  type="button" href="">Tiếp theo</a>
             </div>

88b2e3eee249fb377005910539a27d139aa60eac
Quốc Bình <tranaiquoc4499@gmail.com>
2021-08-27 23:47:59 +0700

    -

--- platform/plugins/car/helpers/helper.php
+++ platform/plugins/car/helpers/helper.php
@@ -3,52 +3,55 @@
 use Illuminate\Database\Eloquent\Collection;
 use Platform\Car\Repositories\Interfaces\CarInterface;
 
-if(!function_exists('get_car_lines')){
-   function get_vehicles($brand = null){
+if (!function_exists('get_car_lines')) {
+   function get_vehicles($brand = null)
+   {
       $vehiclesInterface = resolve('Platform\Vehicle\Repositories\Interfaces\VehicleInterface');
-      if($brand){
+      if ($brand) {
          $slugRepository = resolve('Platform\Slug\Repositories\Interfaces\SlugInterface');
          $slug = $slugRepository->getFirstBy(['key' => $brand, 'reference_type' => \Platform\Brand\Models\Brand::class]);
-        if (!$slug) {
+         if (!$slug) {
             return collect();
-        }
-        $vehicleModel = new Platform\Vehicle\Models\Vehicle;
-        $vehicleModel = $vehicleModel->whereHas('brands',function($q) use ($slug){
-            return $q->where('brand_id',$slug->reference_id);
-        });
-        return $vehiclesInterface->applyBeforeExecuteQuery($vehicleModel)->get();
+         }
+         $vehicleModel = new Platform\Vehicle\Models\Vehicle;
+         $vehicleModel = $vehicleModel->whereHas('brands', function ($q) use ($slug) {
+            return $q->where('brand_id', $slug->reference_id);
+         });
+         return $vehiclesInterface->applyBeforeExecuteQuery($vehicleModel)->get();
       }
       return $vehiclesInterface->all();
    }
 }
-if(!function_exists('get_cars')){
-   function get_cars($brand = null, $vehicle = null){
+if (!function_exists('get_cars')) {
+   function get_cars($brand = null, $vehicle = null)
+   {
       $carInterface = resolve('Platform\Car\Repositories\Interfaces\CarInterface');
       $slugRepository = resolve('Platform\Slug\Repositories\Interfaces\SlugInterface');
       $carModel = new \Platform\Car\Models\Car;
-      if($brand){
+      if ($brand) {
          $slug = $slugRepository->getFirstBy(['key' => $brand, 'reference_type' => \Platform\Brand\Models\Brand::class]);
-         if($slug){
-            $carModel = $carModel->where('brand_id',$slug->reference_id);
+         if ($slug) {
+            $carModel = $carModel->where('brand_id', $slug->reference_id);
          }
       }
-      if($vehicle){
+      if ($vehicle) {
          $slug = $slugRepository->getFirstBy(['key' => $vehicle, 'reference_type' => \Platform\Vehicle\Models\Vehicle::class]);
-         if($slug){
-            $carModel = $carModel->where('vehicle_id',$slug->reference_id);
+         if ($slug) {
+            $carModel = $carModel->where('vehicle_id', $slug->reference_id);
          }
       }
       return $carInterface->applyBeforeExecuteQuery($carModel)->get();;
    }
 }
 
-if(!function_exists('get_car_by_slug')){
-   function get_car_by_slug($slug){
+if (!function_exists('get_car_by_slug')) {
+   function get_car_by_slug($slug)
+   {
       $carInterface = resolve('Platform\Car\Repositories\Interfaces\CarInterface');
       $slugRepository = resolve('Platform\Slug\Repositories\Interfaces\SlugInterface');
       $slug = $slugRepository->getFirstBy(['key' => $slug, 'reference_type' => Platform\Car\Models\Car::class]);
       if ($slug) {
-          return $carInterface->getFirstBy(['id' => $slug->reference_id]);
+         return $carInterface->getFirstBy(['id' => $slug->reference_id]);
       }
       return null;
    }

032c62a87102c0aaf52a6fa7a2e14bf23da3ecb0
Quốc Bình <tranaiquoc4499@gmail.com>
2021-08-28 00:35:46 +0700

    -

--- platform/themes/main/partials/templates/car-selection-menu.blade.php
+++ platform/themes/main/partials/templates/car-selection-menu.blade.php
@@ -3,17 +3,17 @@
         <ul class="step-menu">
             <li class="step-menu__item item-1 {{ url('car-selection') == URL::current() ? 'active' : '' }}">
                 <a href="{{ url('car-selection') }}" class="font18 font-pri text-uppercase color-gray">
-                    1. LỰA CHỌN XE
+                    {{ __("1. LỰA CHỌN XE") }}
                 </a>
             </li>
             <li class="step-menu__item item-2 {{ url('du-toan-chi-phi') == URL::current() ? 'active' : '' }}">
                 <a href="{{ url('du-toan-chi-phi') }}" class=" font18 font-pri text-uppercase color-gray">
-                    2. DỰ TOÁN CHI PHÍ
+                    {{ __("2. DỰ TOÁN CHI PHÍ") }}
                 </a>
             </li>
             <li class="step-menu__item item-3 {{ url('dat-coc') == URL::current() ? 'active' : '' }}">
                 <a href="{{ url('dat-coc') }}" class="font18 font-pri text-uppercase color-gray">
-                    3. ĐẶT CỌC ĐĂNG KÝ
+                    {{ __("3. ĐẶT CỌC ĐĂNG KÝ") }}
                 </a>
             </li>
         </ul>

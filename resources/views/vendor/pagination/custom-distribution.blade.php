88a56bc21d06367d57a2c2b497dc23e8b008b4bb
Trúc Huỳnh <dntrh0425@gmail.com>
2021-08-30 18:17:06 +0700

    sass

--- resources/views/vendor/pagination/custom-distribution.blade.php
+++ resources/views/vendor/pagination/custom-distribution.blade.php
@@ -1,20 +1,20 @@
 @if ($paginator->hasPages())
     <div>
-        <ul class="nav-pagination font18 mb-0">
+        <ul class="nav-pagination font15 mb-0">
             {{-- Pagination Elements --}}
             @foreach ($elements as $element)
                 {{-- "Three Dots" Separator --}}
                 @if (is_string($element))
-                    <li class="page-item active" aria-disabled="true"><span>{{ $element }}</span></li>
+                    <li class="page-item active fontmb-little" aria-disabled="true"><span>{{ $element }}</span></li>
                 @endif
 
                 {{-- Array Of Link --}}
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                            <li class="page-item active fontmb-little" aria-current="page"><span class="page-link">{{ $page }}</span></li>
                         @else
-                            <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+                            <li class="page-item fontmb-little"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
                         @endif
                     @endforeach
                 @endif
@@ -22,19 +22,19 @@
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
-                <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')"> > </a>
+                <li class="page-item fontmb-little">
+                    <a class="page-link fontmb-little" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')"> > </a>
                 </li>
             @else
-                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-                    <span class="page-link" aria-hidden="true"> > </span>
+                <li class="page-item disabled fontmb-little" aria-disabled="true" aria-label="@lang('pagination.next')">
+                    <span class="page-link fontmb-little" aria-hidden="true"> > </span>
                 </li>
             @endif
 
             @if($paginator->lastPage() == $paginator->currentPage())
-                <li class="page-item"> <span class="page-link">>></span> </li>
+                <li class="page-item fontmb-little"> <span class="page-link">>></span> </li>
             @else 
-                <li class="color page-item"> <a class="page-link" href="{{$elements[0][$paginator->lastPage()]}}"> >> </a> </li>
+                <li class="color page-item fontmb-little"> <a class="page-link" href="{{$elements[0][$paginator->lastPage()]}}"> >> </a> </li>
             @endif
         </ul>
     </div>
